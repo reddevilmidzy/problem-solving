7d272bb3af721fcdfb32b9fa5f99c68b2bfe82a0
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-04-26 00:36:35 +0900

    [Bronze I] Title: 더하기 사이클, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,11 +1,11 @@
 N = input()
-count_number = 0
+cnt = 0
 if len(N) == 1:
   N = "0"+N
 a = N
 while True:
   N = N[-1]+(str(int(N[0]) + int(N[1])))[-1]
-  count_number += 1
+  cnt += 1
   if N == a:
-    print(count_number)
+    print(cnt)
     break
