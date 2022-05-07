207b50629b8e69de5543dc5299a6bacc4caf0668
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-07 23:44:27 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 128 ms, Memory: 32404 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -12,13 +12,11 @@ for i in word:
     elif i == ">":
         print(i, end="")
         right = False
-    elif right == False and i != " ":
-        revers.append(i)
+    elif right == False and i != " ": revers.append(i)
     elif right == False and i == " ":
         print("".join(revers)[::-1], end=" ")
         revers = []
-    elif right == True:
-        print(i, end="")
+    elif right == True: print(i, end="")
 
 if len(revers) != 0:
     print("".join(revers)[::-1])
