1e91e3581719cf42b6d13f083bc1b7b3af5cb6aa
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-10 12:51:00 +0900

    [Bronze I] Title: 평균, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,13 +1,6 @@
-N = int(input())
-a = (input().split())
-ave = 0
-for i in range(N):
-  a[i] = int(a[i])
-max_num = max(a)
-
-for i in range(N):
-  a[i] = a[i]/max_num*100
-
-for i in range(N):
-  ave = ave+a[i]  
-print(ave/N)
+n = int(input())
+scores = list(map(int, input().split()))
+max_score = max(scores)
+for i in range(n):
+    scores[i] = scores[i]/max_score*100
+print(sum(scores)/n)
