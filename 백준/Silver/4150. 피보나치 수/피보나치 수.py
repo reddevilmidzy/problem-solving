60cf335486c72be5e76af41d643a9c8a2e15dbc4
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-11 01:40:01 +0900

    [Silver V] Title: 피보나치 수, Time: 68 ms, Memory: 31860 KB -BaekjoonHub

---
+++ 백준/Silver/4150. 피보나치 수/피보나치 수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = [0,1]
+if n == 1 or n ==0:
+    print(a[n])
+else:
+    for i in range(2, n+1):
+        a.append(a[i-1] + a[i-2])
+    print(a[-1])
