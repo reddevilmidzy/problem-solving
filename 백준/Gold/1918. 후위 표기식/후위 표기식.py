2e1838c3689db5b8d560e9bcdc9cd968184b0f73
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-16 20:45:09 +0900

    [Gold III] Title: 후위 표기식, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,21 @@
+pieonsan = {"/":2, "*":2, "+":1, "-":1, "(":0}
+stk = []
+sick = list(input())
+
+for i in sick:
+    if i.isalpha():
+        print(i, end="")
+    elif i == "(":
+        stk.append(i)
+    elif i ==")":
+        while True:
+            tmp = stk.pop()
+            if tmp == "(":
+                break
+            print(tmp, end="")
+    else:
+        while stk and pieonsan[stk[-1]] >= pieonsan[i]:
+            print(stk.pop(), end="")
+        stk.append(i)
+while stk:
+    print(stk.pop(), end="")
