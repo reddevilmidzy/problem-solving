3adfdda34d6393dbc8f0f5fc17c579d24d35b45e
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-19 22:17:14 +0900

    [Bronze II] Title: OX퀴즈, Time: 84 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,16 @@
+test_case = int(input()) # 몇개 오는지 확인
+ox = [] # ox 담을거
+for i in range(test_case):
+  xoxo = input()
+  ox.append(xoxo)
+  long = (len(ox[i]))
+  plus = 0
+  score = []
+  for u in range(long):
+    if (ox[i])[u] == "X":
+      score.append(0)
+      plus = 0
+    elif (ox[i])[u] == "O":
+      plus += 1
+      score.append(plus)
+  print(sum(score))
