2522f2756c843acd54570bd6c664a759b60f017f
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-22 21:35:14 +0900

    [Silver IV] Title: 베스트셀러, Time: 72 ms, Memory: 34004 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,21 @@
+# dict를 사용한 문제
+import sys
+from unicodedata import name
+
+input = sys.stdin.readline
+
+books = {}
+for i in range(int(input())):
+    name = input().rstrip()
+    if name in books:
+        books[name] += 1
+    else:
+        books[name] = 1
+    
+max_val = max(books.values())
+arr = []
+for k, v in books.items():
+    if v == max_val:
+        arr.append(k)
+arr.sort()
+print(arr[0])
