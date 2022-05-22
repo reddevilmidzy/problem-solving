dc8dc1215b6a185c2f3a0ec9d3f0fe7057dada1a
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-05-22 20:04:13 +0900

    [Silver V] Title: 수 정렬하기 3, Time: 9780 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+num = int(input())
+arr = [0]*10000
+# print(arr)
+for i in range(num):
+    a = int(input())
+    arr[a-1] += 1
+
+for i in range(10000):
+    if arr[i] != 0:
+        for j in range(arr[i]):
+            print(i+1)
