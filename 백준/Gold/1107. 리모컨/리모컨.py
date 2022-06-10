f11d930c67803140f3b9ff1bc2b95fb62a89c73d
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-10 16:50:14 +0900

    [Gold V] Title: 리모컨, Time: 1736 ms, Memory: 42596 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+now = 100
+candi = [abs(n-now)]
+if n == 100:
+    if m != 0:
+        map(int,input().rstrip().split())
+    candi.append(0)
+elif m == 0:
+    candi.append(len(str(n)))
+elif m == 10:
+    map(int,input().rstrip().split())
+
+else:
+    breakup = set(map(int,input().split()))
+    if len(str(n)) == 1 and n not in breakup:
+        candi.append(1)
+        pass
+
+    left = False
+    right = False
+    for i in range(500001):
+        plus = set(map(int,str(n+i)))
+        if len(plus) == len(plus-breakup):
+            candi.append(len(str(n+i))+i)
+            right = True
+        if 0 <= n-i:
+            minus = set(map(int,str(n-i)))
+            if len(minus) == len(minus-breakup):
+                candi.append(len(str(n-i))+i)
+                left = True
+        if left and right:
+            break         
+
+print(min(candi))
+
+"""반례
+9990
+8
+1 2 3 4 5 6 7 8 해결
+"""
