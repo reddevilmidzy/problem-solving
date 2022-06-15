ca92a3ead0a2474bc49582bc4944f1cdd451b524
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-15 21:29:34 +0900

    [Gold V] Title: ABCDE, Time: 2908 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**4)
+input = sys.stdin.readline
+
+def dfs(graph, v, visited, cnt):
+    global ans
+    if cnt == 4:
+        ans = 1
+        return 
+    for i in graph[v]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(graph, i, visited,cnt+1)
+            visited[i] = False
+
+n,m = map(int,input().rstrip().split())
+graph = [[] for i in range(n)]
+ans = 0
+for _ in range(m):
+    a,b= map(int,input().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+#print(graph)
+if m >= 4:
+    for node in range(n):
+        visited = [False] * n
+        visited[node] = True
+        dfs(graph, node, visited, 0)
+        if ans == 1:
+            break
+print(ans)
