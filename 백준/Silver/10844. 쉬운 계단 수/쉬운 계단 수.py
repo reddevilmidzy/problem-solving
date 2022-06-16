a3db3fee8f9ccb63b7f4daded1f15dd140ad750e
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-16 22:21:24 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,15 @@
+n = int(input())
+answer = [[], [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+for i in range(2, n+1):
+    temp = []
+    for j in range(10):
+        if j == 0:
+            temp.append(answer[i - 1][1])
+        elif j == 9:
+            temp.append(answer[i - 1][8])
+        else:
+            temp.append(answer[i - 1][j - 1] + answer[i - 1][j + 1])
+    answer.append(temp)
+
+print(sum(answer[n])%1000000000)
