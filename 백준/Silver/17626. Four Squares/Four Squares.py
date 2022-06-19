42f46e026d573caa1b8d574ddeabc11520198c4a
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-20 00:35:49 +0900

    [Silver IV] Title: Four Squares, Time: 172 ms, Memory: 114924 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,7 +1,7 @@
 n = int(input())
 dp = [i for i in range(n+1)]
 for i in range(1,n+1):
-    for j in range(1,int(n**0.5)+1):
+    for j in range(1,int(i**0.5)+1):
         if 0<=i-j**2:
             dp[i] = min(dp[i-j**2]+1, dp[i])
 print(dp[n])
