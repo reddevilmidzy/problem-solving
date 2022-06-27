4d48ab4ea2c5d676718c7bf0c811766c3d5da4dd
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-27 11:11:01 +0900

    [Gold V] Title: Hello World!, Time: 516 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/13140. Hello World！/Hello World！.py
+++ 백준/Gold/13140. Hello World！/Hello World！.py
@@ -5,10 +5,12 @@ nums = [1,2,3,4,5,6,7,8,9,0]
 n = int(input().rstrip())
 for s in itertools.permutations(nums, 7):
     h,e,l,o,w,r,d = s
+    if h == 0 or w == 0:
+        continue
     hello = 10000*h + 1000*e + 100*l + 10*l + o
     world = 10000*w + 1000*o + 100*r + 10*l + d
     
-    if hello + world == n and h != 0 and w != 0:
+    if hello + world == n:
         print(f"  {hello}")
         print(f"+ {world}")
         print('-'*7)
