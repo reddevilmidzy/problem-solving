d5e3ba66a7a8206a966d7008bb221c77def2b2a4
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-06-29 23:22:17 +0900

    [Silver I] Title: IPv6, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/3107. IPv6/IPv6.py
@@ -0,0 +1,13 @@
+ip = list(input().split(':'))
+
+if ip.count(''):
+    while len(ip) > 8:
+        del ip[ip.index('')]
+    while len(ip) < 8:
+        ip.insert(ip.index(''), '0000')
+    
+for i in range(8):
+    if len(ip[i]) < 4:
+        ip[i] = '0'*(4-len(ip[i])) + ip[i]
+
+print(*ip, sep=':')
