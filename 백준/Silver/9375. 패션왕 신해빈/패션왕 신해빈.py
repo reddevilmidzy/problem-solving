a956a43dba8022ecda9fd5977569b39ac4df0b49
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-07 08:20:26 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -7,11 +7,11 @@ for _ in range(int(input())):
     cabinet = dict()
     for i in range(n):
         name, frac = map(str,input().rstrip().split())
-        if frac not in cabinet:
-            cabinet[frac] = [name]
+        if frac not in cabinet.keys():
+            cabinet[frac] = 1
         else:
-            cabinet[frac].append(name)
+            cabinet[frac] += 1
     #print(cabinet)
     for k in cabinet.keys():
-        ans *= len(cabinet[k])+1
+        ans *= cabinet[k]+1
     print(ans-1)
