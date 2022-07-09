99099708b5a809a8a7edb468bd4578f468c89296
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-09 09:58:31 +0900

    [Silver I] Title: IOIOI, Time: 316 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,18 @@
+n = int(input())
+m = int(input())
+s = input()
+
+ans = 0
+check = 0
+i = 0
+while i < m-2:
+    if s[i:i+3] == 'IOI':
+        check += 1
+        i += 1
+        if check == n:
+            ans += 1
+            check -= 1
+    else:
+        check = 0
+    i += 1
+print(ans)
