2dee8eeaac8b1f46080a7947e0e2f66d1287729c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-10 18:55:43 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().rstrip().split())
+arr = [i for i in range(1, n+1)]
+pt = 0
+ans = []
+
+for i in range(n):
+    pt += k-1
+    pt %= len(arr)
+    ans.append(arr.pop(pt))
+print(f"<{', '.join(map(str, ans))}>")
