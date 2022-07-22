bd232728741c1162cd6f46edb1bc2a8a62746f1f
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-22 10:04:20 +0900

    [Gold V] Title: 테트로미노, Time: 280 ms, Memory: 118432 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -6,7 +6,7 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int,input().rstrip().split())))
 
-res = []
+res = set()
 
 for i in range(n):
     for j in range(m):
@@ -18,15 +18,15 @@ for i in range(n):
                 now += graph[i+k][j]
 
             if i+3<n:
-                res.append(now+graph[i+3][j])
+                res.add(now+graph[i+3][j])
             if j+1<m:
-                res.append(now+graph[i][j+1])
-                res.append(now+graph[i+1][j+1])
-                res.append(now+graph[i+2][j+1])
+                res.add(now+graph[i][j+1])
+                res.add(now+graph[i+1][j+1])
+                res.add(now+graph[i+2][j+1])
             if j>0:
-                res.append(now+graph[i][j-1])
-                res.append(now+graph[i+1][j-1])
-                res.append(now+graph[i+2][j-1])
+                res.add(now+graph[i][j-1])
+                res.add(now+graph[i+1][j-1])
+                res.add(now+graph[i+2][j-1])
 
         if j+2<m:
             now = 0
@@ -34,28 +34,28 @@ for i in range(n):
                 now += graph[i][j+k]
             
             if j+3<m:
-                res.append(now+graph[i][j+3])
+                res.add(now+graph[i][j+3])
             if i+1<n:
-                res.append(now+graph[i+1][j])
-                res.append(now+graph[i+1][j+1])
-                res.append(now+graph[i+1][j+2])
+                res.add(now+graph[i+1][j])
+                res.add(now+graph[i+1][j+1])
+                res.add(now+graph[i+1][j+2])
             if i>0:
-                res.append(now+graph[i-1][j])
-                res.append(now+graph[i-1][j+1])
-                res.append(now+graph[i-1][j+2])
+                res.add(now+graph[i-1][j])
+                res.add(now+graph[i-1][j+1])
+                res.add(now+graph[i-1][j+2])
 
         # 네모
         if i+1<n and j+1<m:
             now = graph[i][j]+graph[i+1][j]+graph[i][j+1]+graph[i+1][j+1]
-            res.append(now)
+            res.add(now)
         
         if i+1<n and 0<j<m-1:
             now = graph[i][j]+graph[i+1][j]
-            res.append(now+graph[i][j+1]+graph[i+1][j-1])
-            res.append(now+graph[i][j-1]+graph[i+1][j+1])
+            res.add(now+graph[i][j+1]+graph[i+1][j-1])
+            res.add(now+graph[i][j-1]+graph[i+1][j+1])
         if 0<i<n-1 and j+1<m:
             now = graph[i][j]+graph[i][j+1]
-            res.append(now+graph[i+1][j]+graph[i-1][j+1])
-            res.append(now+graph[i-1][j]+graph[i+1][j+1])
+            res.add(now+graph[i+1][j]+graph[i-1][j+1])
+            res.add(now+graph[i-1][j]+graph[i+1][j+1])
                 
 print(max(res))
