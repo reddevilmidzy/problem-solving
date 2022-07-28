34bf106340d2754fbf2eade97a0b938a0a815553
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-28 09:48:45 +0900

    [Gold V] Title: 두 용액, Time: 164 ms, Memory: 41532 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,11 +1,14 @@
 n=int(input())
 nums=sorted(list(map(int,input().split())))
 f_idx, b_idx=0,n-1
-ans=[]
+ans=[2000000000,0,0]
 
 while f_idx<b_idx:
     solution=nums[f_idx]+nums[b_idx]
-    ans.append([abs(solution),nums[f_idx],nums[b_idx]])
+    if ans[0]>abs(solution):
+        ans[0]=abs(solution)
+        ans[1]=nums[f_idx]
+        ans[2]=nums[b_idx]
     if solution <0:
         f_idx+=1
     elif solution>0:
@@ -13,5 +16,4 @@ while f_idx<b_idx:
     else:
         print(nums[f_idx], nums[b_idx])
         exit()
-ans.sort()
-print(ans[0][1],ans[0][2])
+print(ans[1],ans[2])
