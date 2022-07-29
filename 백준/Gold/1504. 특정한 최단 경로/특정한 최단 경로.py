4069788d59da5482349049a00433c88cebfe0568
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-29 14:44:22 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 856 ms, Memory: 68828 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,31 @@
+import sys, heapq
+input = sys.stdin.readline
+
+def dijkstra(start, end):
+    dis=[0xffffff]*(n+1)
+    dis[start]=0
+    q = [[0,start]]
+    while q:
+        k,u = heapq.heappop(q)
+        if k > dis[u]:
+            continue
+        for w,v in graph[u]:
+            if dis[v] > dis[u] + w:
+                dis[v] = dis[u]+w
+                heapq.heappush(q, [dis[v],v])
+    return dis[end]
+
+n,e=map(int,input().rstrip().split())
+graph = [[] for _ in range(n+1)]
+for i in range(e):
+    a,b,c=map(int,input().rstrip().split())
+    graph[a].append([c,b])
+    graph[b].append([c,a])
+
+first_stop, second_stop=map(int,input().rstrip().split())
+
+first_path = dijkstra(1, first_stop)+dijkstra(first_stop, second_stop)+dijkstra(second_stop,n)
+second_path = dijkstra(1, second_stop)+dijkstra(second_stop, first_stop)+dijkstra(first_stop,n)
+
+
+print(-1 if first_path>=0xffffff and second_path>=0xffffff else min(first_path, second_path))
