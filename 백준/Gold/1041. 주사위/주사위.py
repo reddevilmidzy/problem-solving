80dcde2ec58aca81d18409c273ef3015ca87e447
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-20 16:31:29 +0900

    [Gold V] Title: 주사위, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1041. 주사위/주사위.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a,b,c,d,e,f = map(int,input().split())
+
+center = min(a,b,c,d,e,f)
+edge = min(a+b, a+c, a+e, a+d, f+c, f+e, f+d, f+b, c+b, b+d, d+e, e+c)
+coner = min(a+c+e, a+b+c, a+b+d, a+d+e, f+b+c, f+b+d, f+d+e, f+e+c)
+
+if n == 1:
+    print(a+b+c+d+e+f-max(a,b,c,d,e,f))
+else:
+    n -= 2
+    print((center*(n**2)*5)+4*(n*center)+coner*4+edge*(n*8+4))
