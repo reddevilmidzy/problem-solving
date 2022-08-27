a944c667f63b25dcde477b2ae4214285f2c526db
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-27 16:15:47 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 152 ms, Memory: 122224 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,12 +1,13 @@
 n,m = map(int,input().split())
-
-sqrt_nums = [i**2 for i in range(2, int(m**0.5)+1)]
+last = int(m**0.5)
 arr = [1]*(m-n+1)
-
-for i in sqrt_nums:
-    tmp = (n//i)*i # 시작값 변경 가능할 듯
+sqr = 2
+while sqr <= last:
+    i = sqr**2
+    tmp = (n//i)*i
     while tmp<=m:
         if n <= tmp and tmp <= m:
             arr[tmp-n] = 0
         tmp += i
+    sqr += 1
 print(sum(arr))
