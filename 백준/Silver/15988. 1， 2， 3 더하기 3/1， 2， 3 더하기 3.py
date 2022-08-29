272240c97f894dedaf756c1a1a0392dada12fa52
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-29 19:28:42 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 124 ms, Memory: 122164 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,17 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-for _ in range(int(input())):
-    n = int(input())
-    if n < 4:
-        dp = [0,1,2,4]
-        
-    else:
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
+nums = [int(input()) for _ in range(int(input()))]
+max_num = max(nums)
+if max_num < 4:
+    dp = [0,1,2,4]
+else:
+    dp = [0]*(max_num+1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    for i in range(4, max_num+1):
+        dp[i] = (dp[i-3]+dp[i-2]+dp[i-1])%1000000009
 
-        for i in range(4, n+1):
-            dp[i] = (dp[i-3]+dp[i-2]+dp[i-1])%1000000009
-    print(dp[n])
+for k in nums:
+    print(dp[k])
