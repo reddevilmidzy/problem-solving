85818beb9a1896a2a31027af719194f432cdbc77
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-02 00:17:32 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 120 ms, Memory: 114408 KB -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,9 +1,9 @@
 n=int(input())
-dp = [0,1]
-if n <= 1:
+dp = [0,1,1]
+if n <= 2:
     print(dp[n])
 else:
-    for i in range(2,n+1):
+    for i in range(3,n+1):
         x=dp.pop()
         y=dp.pop()
         dp.append(x)
