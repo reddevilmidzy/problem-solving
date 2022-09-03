0f050a9ec6250656daa76621bc2f2fa5d1af1ca8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-03 21:36:01 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 940 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,18 +1,12 @@
 def hanoi_top_move(n, start, end):
-    if n == 1:
+    if n == 1: # 마지막 원판
         print(start, end)
         return
     
-    hanoi_top_move(n-1, start, 6-start-end)
+    hanoi_top_move(n-1, start, 6-start-end) # 시작점에서 현재 시작점도 아니고 끝점도 아닌 마지막한곳
     print(start, end)
     hanoi_top_move(n-1, 6-start-end, end)
 
-def hanoi_top_cnt(n):
-    if n == 1:
-        return 1
-    else:
-        return hanoi_top_cnt(n-1)*2 + 1
-
 n = int(input())
-print(hanoi_top_cnt(n))
+print(2**n-1)
 hanoi_top_move(n, 1, 3)
