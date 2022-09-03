78d979442dcf46ba9fb41ad19bbf03949dc942dc
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-03 22:23:48 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 188 ms, Memory: 116792 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -11,16 +11,20 @@ for _ in range(int(input())):
     else:
         high = nums[-1]
         low = nums[0]
+        # 배열의 처음 값은 가장 낮은 값
         res[0] = low
+        # 배열의 중간 값은 가장 높은 값
         if n%2==0: 
             res[n//2]=high
         else:
             res[n//2+1]=high
         idx = 1
+        
         for i in range(1, n-1,2):
             res[idx] = nums[i]
             idx +=1
         idx += 1
+        # 숫자 조정
         if i==n-2:
             i -=1
         elif i==n-3:
@@ -28,7 +32,7 @@ for _ in range(int(input())):
         for i in range(i,1,-2):
             res[idx] = nums[i]
             idx += 1
-
+        # 초기 ans 값은 처음과 마지막값
         ans = res[-1]-res[0]
         for i in range(1,n):
             ans = max(ans, abs(res[i]-res[i-1]))
