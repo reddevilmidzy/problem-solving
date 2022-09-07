0f026e52f29315c65f1893b20293ee9ef3b3e98e
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-07 18:41:57 +0900

    [Silver III] Title: 다음 순열, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.py
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -3,31 +3,22 @@ input = sys.stdin.readline
 
 n = int(input())
 k = list(map(int,input().rstrip().split()))
-nums = [i for i in range(1,n+1)]
 ans= []
 if k == sorted(k)[::-1]:
     print(-1)
 else:
     for i in range(n-1, 0, -1):
-        #print(i)
         if k[i] > k[i-1]:
-            # print(k[i], k[i-1])
-            #if k[:i-1] != []:
             ans.extend(k[:i-1])
-            #print(k[:i-1],end=' ')
-            #else:
-
             tmp = k[i:]
             check = sorted(k[i:])
 
             for j in range(len(check)):
                 if check[j] > k[i-1]:
                     ans.append(check[j])
-                    # print(check[j],end=' ')
                     tmp.remove(check[j])
                     break
             tmp.append(k[i-1])
             ans.extend(sorted(tmp))
-            # print(sorted(tmp))
             break
     print(*ans)
