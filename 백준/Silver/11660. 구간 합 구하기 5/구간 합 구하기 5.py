d6594b7aecb32ca53e99b502e43953a64260d0e6
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-10 00:15:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1360 ms, Memory: 129620 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+coor = [list(map(int,input().split())) for _ in range(m)]
+
+prefix = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        # print(f'{graph[i-1][j-1]}+{prefix[i][j-1]}+{prefix[i-1][j]}-{prefix[i-1][j-1]}')
+        prefix[i][j] = graph[i-1][j-1]+prefix[i][j-1]+prefix[i-1][j]-prefix[i-1][j-1]
+
+for x1,y1,x2,y2 in coor:
+    if x1==x2 and y1==y2:
+        print(graph[x1-1][y1-1])
+    else:
+        ans = prefix[x2][y2]-prefix[x1-1][y2]-prefix[x2][y1-1]+prefix[x1-1][y1-1]
+        print(ans)
