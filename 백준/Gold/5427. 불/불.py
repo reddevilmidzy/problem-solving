dd98028a1e5b46448d789fd2ea5e3a3a066e2bd8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-11 12:50:12 +0900

    [Gold IV] Title: 불, Time: 4608 ms, Memory: 77180 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.py
@@ -0,0 +1,58 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [0,1,-1,0]
+dy = [1,0,0,-1]
+
+def burn():
+    # queue.append([a,b,1])
+    while queue:
+        x,y,t = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= h or ny >= w:
+                continue
+            if graph[nx][ny] != '#' and visited[nx][ny] == 0:
+                visited[nx][ny] = t + 1
+                queue.append([nx,ny,t+1])
+    return visited
+
+
+def bfs(a,b,v):
+    q = deque()
+    q.append([a,b,1])
+    while q:
+        x,y,c = q.popleft()
+        if x == h-1 or y == w-1 or x == 0 or y == 0:
+            return c
+
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if nx < 0 or ny < 0 or nx >= h or ny >= w:
+                continue
+            if graph[nx][ny] != '#':
+                if (c+1 < visit[nx][ny] and visit[nx][ny] != -1) or visit[nx][ny] == 0: 
+                    q.append([nx,ny,c+1])
+                    visit[nx][ny] = -1
+    return 'IMPOSSIBLE'
+for _ in range(int(input())):
+    w,h = map(int,input().split())
+    graph = [list(map(str,input().rstrip())) for _ in range(h)]
+    queue = deque()
+    visited = [[0]*(w) for _ in range(h)]
+    # sx,sy = 0,0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == '*':
+                queue.append([i,j,1])
+                visited[i][j] = 1
+            elif graph[i][j] == '@':
+                sx,sy = i,j
+    visit = burn()
+    print(bfs(sx,sy,visit))
+    
