1180982ca917aa16bc37865048586c2ce99eeec1
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-13 20:02:42 +0900

    [Gold V] Title: 무한 수열, Time: 88 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -7,17 +7,11 @@ dp[0] = 1
 def recursion(n,p,q):
     if n == 0:
         return 1
-    if dp[n//p] != 0 and dp[n//q] != 0:
-        return dp[n//p] + dp[n//q]
-    if dp[n//p] != 0:
-        dp[n//q] = recursion(n//q,p,q)
-        return dp[n//p] + dp[n//q]
-    if dp[n//q] != 0:
-        dp[n//p] = recursion(n//p,p,q)
-        return recursion(n//p,p,q) + dp[n//q]
     
-    dp[n//p] = recursion(n//p,p,q)
-    dp[n//q] = recursion(n//q,p,q)
-    return dp[n//p] + dp[n//q]
+    if dp[n//p]==0:
+        dp[n//p] = recursion(n//p,p,q)
+    if dp[n//q]==0:
+        dp[n//q] = recursion(n//q,p,q)
+    return dp[n//p]+dp[n//q]
 
 print(recursion(n,p,q))
