1aaa24ef761a4757330bf8f508de9fdcb0a0c1a0
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-17 12:54:11 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 360 ms, Memory: 55632 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,48 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+def dijkstra(start,end):
+    INF = int(1e9)
+    distance = [INF]*(n+1)
+    distance[start] = 0
+    global root
+    h = []
+    heapq.heappush(h,(start,0))
+    while h:
+        node, dist = heapq.heappop(h)
+        if distance[node] < dist:
+            continue
+
+        for u,v in graph[node]:
+            if dist+v < distance[u]:
+                # print('node',node, 'u',u)
+                root[u] = [node]
+                distance[u] = dist+v
+                heapq.heappush(h, (u, dist+v))
+    # print('root',root)
+    return distance[end]
+
+def dfs(start):
+    global route
+    route.append(start)
+    for i in root[start]:
+        # route.append(start)
+        # print(i)
+        dfs(i)
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+root = [[] for _ in range(n+1)]
+route = []
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+
+start, end= map(int,input().split())
+
+print(dijkstra(start, end))
+dfs(end)
+print(len(route))
+print(*reversed(route))
