cb6990cf8224f41132df501070b47c2a842724b8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-18 02:09:04 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -4,31 +4,24 @@ input = sys.stdin.readline
 n = int(input())
 graph = [list(map(int,input().split())) for _ in range(n)]
 
+# [가로,세로,대각선] 의 방문 개수
 dp = [[[0,0,0] for _ in range(n)] for _ in range(n)]
+# 시작은 가로(끝점에만 기록)
 dp[0][1][0] = 1
-#print(dp)
 
 for i in range(n):
-    for j in range(n):
+    for j in range(1,n):
+		# 이전에 방문한 곳만 방문 가능
         if sum(dp[i][j]) != 0:
-            first = False
-            second = False
-            three = False
+            row = False
+            col = False
             if j+1 < n and graph[i][j+1]!=1:
-                first = True
+                col = True
                 dp[i][j+1][0] = dp[i][j][0]+dp[i][j][2]
-                #print('chekc')
             if i+1 < n and graph[i+1][j]!=1:
-                second = True
+                row = True
                 dp[i+1][j][1] = dp[i][j][1]+dp[i][j][2]
-                #print('chekc2')
-            if i+1<n and j+1<n and graph[i+1][j+1]!=1 and first and second:
-                dp[i+1][j+1][2] = dp[i][j][2]
-                three = True
-                #print('chekc3')
-            
-            if three:
-                #print('chekaaaaa')
+            if row and col and graph[i+1][j+1]!=1:
                 dp[i+1][j+1][2] = dp[i][j][0]+dp[i][j][1]+dp[i][j][2]
-# print(dp)    
+                
 print(sum(dp[n-1][n-1]))
