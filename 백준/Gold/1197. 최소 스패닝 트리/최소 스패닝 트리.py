803e88b218d4a199d4437fbb36a2e434a4c40e03
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-19 23:18:10 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 468 ms, Memory: 50076 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,36 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+    
+
+
+
+v,e = map(int,input().split())
+graph = [[] for _ in range(v+1)]
+dist = [False]*(v+1)
+edges = []
+parent = [i for i in range(v+1)]
+ans = 0
+for _ in range(e):
+    a,b,c = map(int,input().split())
+    heapq.heappush(edges, (c,a,b))
+
+while edges:
+    cost,a,b = heapq.heappop(edges)
+    if find_parent(parent,a) != find_parent(parent,b):
+        union_parent(parent,a,b)
+        ans += cost
+print(ans)
