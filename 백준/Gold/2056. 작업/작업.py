9d1502193be4d93ca561a11a3ee8d2dbbd22f445
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-20 12:38:27 +0900

    [Gold V] Title: 작업, Time: 632 ms, Memory: 35960 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n= int(input())
+cost = [0]
+deg = [0]*(n+1)
+ans = [0]*(n+1)
+graph = [[] for _ in range(n+1)]
+queue = deque()
+for i in range(1,n+1):
+    nums = list(map(int,input().split()))
+    cost.append(nums[0])
+    for j in range(2,nums[1]+2):
+        deg[i] += 1
+        #print('i',i,'j',j)
+        graph[nums[j]].append(i)
+
+
+for i in range(1,n+1):
+    if deg[i] == 0:
+        queue.append(i)
+
+while queue:
+    node = queue.popleft()
+    ans[node] += cost[node]
+    for i in graph[node]:
+        deg[i] -= 1
+        ans[i] = max(ans[i], ans[node])
+        if deg[i]==0:
+            queue.append(i)
+print(max(ans))
