1eb856e149a5e0d1fb2f82061cd62ed3549640a9
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-24 20:22:23 +0900

    [Silver III] Title: 에너지 드링크, Time: 92 ms, Memory: 42312 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.py
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.py
@@ -1,11 +1,5 @@
 import sys
 input = sys.stdin.readline
-
-n=  int(input())
-nums= sorted(list(map(int,input().split())))
-ans = 0
-sum_ans = sum(nums[:n-1])
-if sum_ans%2==0:
-    print(sum_ans//2+nums[n-1])
-else:
-    print(sum_ans/2+nums[n-1])
+n=int(input())
+nums= list(map(int,input().split()))
+print(sum(nums)/2+max(nums)/2)
