381053b99e5e8b44a10cae6711aeb8795d8f7c26
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-24 23:26:43 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2524 ms, Memory: 61232 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int,input().split()))
+dp = [[0]*(n) for _ in range(n)]
+
+
+# for i in range(n):
+#     dp[i][i] = 1
+
+for i in range(n):
+    for start in range(n-i):
+        end = start + i
+        if start==end:
+            dp[start][end]=1
+        elif nums[start] == nums[end]:
+            if start+1==end:
+                dp[start][end]=1
+            elif dp[start+1][end-1] == 1:
+                dp[start][end] = 1
+
+
+
+m = int(input())
+for _ in range(m):
+    a,b = map(int,input().split())
+    print(dp[a-1][b-1])
