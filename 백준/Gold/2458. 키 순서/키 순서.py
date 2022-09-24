056c4475680a433e74279ea371b04a764f57634d
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-24 13:35:30 +0900

    [Gold IV] Title: 키 순서, Time: 1116 ms, Memory: 117156 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,30 @@
+# 위상정렬로도 풀어보기
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n,m = map(int,input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int,input().split()) 
+    graph[a][b] = 1
+
+for i in range(1,n+1):
+    graph[i][i] = 0
+
+for x in range(1,n+1):
+    for i in range(1, n+1):
+        for j in range(1,n+1):
+            if graph[i][j] > graph[i][x]+graph[x][j]:
+                graph[i][j] = graph[i][x]+graph[x][j]
+
+ans = 0         
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j]==INF:
+            if graph[j][i]==INF:
+                break
+    else:
+        ans += 1
+print(ans)
