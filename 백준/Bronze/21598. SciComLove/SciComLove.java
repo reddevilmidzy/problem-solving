06cf4544f6b232d7a82a07d4c6d92ee9cf57221e
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-10-13 12:26:55 +0900

    [Bronze V] Title: SciComLove, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<n; i++){
+            System.out.println("SciComLove");
+        }
+    }
+}
