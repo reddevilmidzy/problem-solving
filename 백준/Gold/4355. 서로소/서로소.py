f92b7952ce4575b7e8f2e35e7f6a27fd2535ad43
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-11-22 13:35:44 +0900

    [Gold I] Title: 서로소, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/4355. 서로소/서로소.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def euler_phi(n):
+    i = 2
+    ret = n
+    while i*i <= n:
+        if n%i==0:
+            while n%i==0:
+                n //= i
+            ret -= ret//i
+        i += 1
+    if n > 1:
+        ret -= ret//n
+    
+    return ret
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(euler_phi(n))
