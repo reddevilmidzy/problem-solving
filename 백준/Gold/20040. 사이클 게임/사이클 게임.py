093bb70832836b4be1fd960c1ecdd5bd26870d7d
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-11-25 16:15:23 +0900

    [Gold IV] Title: 사이클 게임, Time: 1080 ms, Memory: 126876 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n,m = map(int,input().split())
+
+parent = [i for i in range(n)]
+flag = False
+ans = 0
+for i in range(1, m+1):
+    u, v = map(int,input().split())
+    if find(parent, u) != find(parent, v):
+        union(parent, u, v)
+    elif not flag:
+        ans = i
+        flag = True
+print(ans)
