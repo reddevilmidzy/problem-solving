f51d5a22ee2844209dc43a04eccd847ab85f5453
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-12-13 10:39:51 +0900

    [Silver V] Title: 카약과 강풍, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2891. 카약과 강풍/카약과 강풍.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n,s,r = map(int, input().split())
+broke = sorted(list(map(int,input().split())))
+redun = sorted(list(map(int,input().split())))
+broke, redun = list(set(broke)-set(redun)), list(set(redun)-set(broke))
+
+ans = 0
+for i in broke:
+    if i-1 in redun:
+        redun.remove(i-1)
+    elif i+1 in redun:
+        redun.remove(i+1)
+    else:
+        ans += 1
+
+print(ans)
