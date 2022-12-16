c4d8065aba498b7e6415d21e238da01a087a57b6
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-12-16 09:51:40 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -1,9 +1,2 @@
 from itertools import combinations_with_replacement
-n = int(input())
-arr = [1,5,10,50]
-res = set()
-
-for i in combinations_with_replacement(arr, n):
-    res.add(sum(list(i)))
-
-print(len(res))
+print(len(set(sum(list(i)) for i in combinations_with_replacement([1,5,10,50], int(input())))))
