c6a3161a7d3736d12b2344d9543e0bfc4c1f4a67
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-12-20 12:20:08 +0900

    [Gold IV] Title: 타일 채우기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,7 +1,5 @@
-n = int(input())
-dp = [0,0,3,0,11] + [0]*(n-4)
-
-for i in range(6, n+1,2):
-    dp[i] = dp[i-2]*4 - dp[i-4]
-
+n=int(input())
+dp=[1,0,3]+[0]*(n-2)
+for i in range(4,n+1,2):
+    dp[i]=dp[i-2]*4-dp[i-4]
 print(dp[n])
