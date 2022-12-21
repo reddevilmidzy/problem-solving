ecf4b22594546e81eda86dea81f45562a1225c6c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-12-21 14:16:32 +0900

    [Gold III] Title: 택배, Time: 1612 ms, Memory: 32232 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n,m = map(int,input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+route = [[j for j in range(n+1)] for i in range(n+1)]
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a][b] = min(graph[a][b], c)
+    graph[b][a] = min(graph[b][a], c)
+
+for k in range(1, n+1):
+    graph[k][k] = 0
+    route[k][k] = "-"
+
+for x in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if graph[i][x] + graph[x][j] < graph[i][j]:
+                graph[i][j] = graph[i][x] + graph[x][j]
+                route[i][j] = route[i][x]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(route[i][j], end=' ')
+    print()
