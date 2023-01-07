0b7b0d7ca5673771e3ee94c02b970d24500c174b
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-01-07 17:49:59 +0900

    [Gold III] Title: 줄 세우기, Time: 200 ms, Memory: 38780 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -4,24 +4,24 @@ input = sys.stdin.readline
 
 n,m = map(int,input().split())
 graph = [[] for _ in range(n+1)]
-deg = [0]*(n+1)
+dag = [0]*(n+1)
 res = []
 for _ in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
-    deg[b] += 1
+    dag[b] += 1
 
 queue = deque()
 for i in range(1, n+1):
-    if deg[i]==0:
+    if dag[i]==0:
         queue.append(i)
-    
+
 while queue:
     node = queue.popleft()
     res.append(node)
     for i in graph[node]:
-        deg[i] -= 1
-        if deg[i] == 0:
+        dag[i] -= 1
+        if dag[i] == 0:
             queue.append(i)
 
 print(*res)
