8ba352c3f64b246180e7ad645b1e2052da957586
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-01-07 18:01:36 +0900

    [Gold II] Title: 문제집, Time: 164 ms, Memory: 40364 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,25 +1,30 @@
 import sys, heapq
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-edges = [0]*(n+1)
-graph = [[] for _ in range(n+1)]
-ans = []
-for _ in range(m):
-    a,b = map(int,input().split())
-    edges[b]+=1
-    graph[a].append(b)
+def solve():
+    n,m = map(int,input().split())
+    edges = [0]*(n+1)
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a,b = map(int,input().split())
+        edges[b]+=1
+        graph[a].append(b)
+    topology(n, graph, edges)
 
-h = []
-for i in range(1, n+1):
-    if not edges[i]:
-        heapq.heappush(h, i)
-
-while h:
-    node = heapq.heappop(h)
-    ans.append(node)
-    for i in graph[node]:
-        edges[i] -= 1
+def topology(n, graph, edges):
+    ans = []
+    h = []
+    for i in range(1, n+1):
         if not edges[i]:
             heapq.heappush(h, i)
-print(*ans)
+
+    while h:
+        node = heapq.heappop(h)
+        ans.append(node)
+        for i in graph[node]:
+            edges[i] -= 1
+            if not edges[i]:
+                heapq.heappush(h, i)
+    print(*ans)
+
+solve()
