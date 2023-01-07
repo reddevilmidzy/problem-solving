4fdd9043699653af810398d937e089b1a395a480
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-01-07 17:57:08 +0900

    [Gold II] Title: 문제집, Time: 200 ms, Memory: 39556 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,24 @@
+import sys, heapq
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+edges = [0]*(n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    edges[b]+=1
+    graph[a].append(b)
+
+h = []
+for i in range(1, n+1):
+    if edges[i] == 0:
+        heapq.heappush(h, i)
+
+while h:
+    node = heapq.heappop(h)
+    print(node, end=' ')
+    for i in graph[node]:
+        edges[i] -= 1
+        if edges[i] == 0:
+            heapq.heappush(h, i)
