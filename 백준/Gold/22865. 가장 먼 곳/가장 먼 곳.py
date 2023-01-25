43f147939c4549ed7b709ae384b049a1427f8796
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-01-25 13:45:27 +0900

    [Gold IV] Title: 가장 먼 곳, Time: 2216 ms, Memory: 212384 KB -BaekjoonHub

--- 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.py
+++ 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 INF = int(1e9)
 
 
-def dijkstra(start):
+def dijkstra(start, n, graph):
     distance = [INF]*(n+1)
     distance[start] = 0
     pq = []
@@ -20,25 +20,24 @@ def dijkstra(start):
 
     return distance
 
-n = int(input())
-nums = list(set(map(int,input().split())))
-graph = [[] for _ in range(n+1)]
-m = int(input())
-distance_list = []
-for _ in range(m):
-    u,v,w = map(int,input().split())
-    graph[u].append((v,w))
-    graph[v].append((u,w))
+def solve():
+    n = int(input())
+    a,b,c = map(int,input().split())
+    graph = [[] for _ in range(n+1)]
+    m = int(input())
+    ans = 0
 
-for i in nums:
-    distance_list.append(dijkstra(i))
+    for _ in range(m):
+        u,v,w = map(int,input().split())
+        graph[u].append((v,w))
+        graph[v].append((u,w))
 
-ans = [0]
+    a_dist = dijkstra(a,n,graph)
+    b_dist = dijkstra(b,n,graph)
+    c_dist = dijkstra(c,n,graph)
+    ans = [0]
+    for i in range(1, n+1):
+        ans.append(min(a_dist[i], b_dist[i], c_dist[i]))
+    print(ans.index(max(ans)))
 
-for i in range(1, n+1):
-    tmp = INF
-    for j in range(len(nums)):
-        tmp = min(tmp, distance_list[j][i])
-    ans.append(tmp)
-
-print(ans.index(max(ans)))
+solve()
