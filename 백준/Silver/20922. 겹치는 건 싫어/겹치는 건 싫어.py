f3ca769cf44e48dad6fc00532f78cec32165d579
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-01 12:48:21 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 184 ms, Memory: 172552 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+nums = list(map(int,input().split()))
+
+arr = [0]*(max(nums)+1)
+ans = [1]
+one,two = 0,0
+
+while two < n:
+    
+    if arr[nums[two]] < k:
+        arr[nums[two]] += 1
+        two += 1
+    else:
+        arr[nums[one]] -= 1
+        one += 1
+
+    ans.append(two-one)
+
+print(max(ans))
