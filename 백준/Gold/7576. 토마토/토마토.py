15d227fba4910e5e27daecf22e698a424bb7d982
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-01 20:41:31 +0900

    [Gold V] Title: 토마토, Time: 1372 ms, Memory: 106844 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,6 +2,14 @@ import sys
 from collections import deque
 input =sys.stdin.readline
 
+def solve():
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 0:
+                return -1
+    return max([max(graph[i]) for i in range(m)]) - 1
+
+
 n,m = map(int, input().rstrip().split())
 graph = [list(map(int,input().rstrip().split())) for _ in range(m)]
 queue = deque()
@@ -16,7 +24,6 @@ for i in range(m):
 def bfs():
     while queue:
         x,y = queue.popleft()
-        #print(x,y)
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
@@ -27,12 +34,4 @@ def bfs():
                     graph[nx][ny] = graph[x][y] + 1
 
 bfs()
-ans = 0
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 0:
-            ans = -1
-            print(ans)
-            exit()
-    ans = max(ans, max(graph[i]))
-print(ans-1)
+print(solve())
