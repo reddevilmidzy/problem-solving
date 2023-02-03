f0a81b37a95f6e91f5cb17fe944ccbeff20aa379
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-03 21:29:09 +0900

    [Gold V] Title: 강의실 배정, Time: 368 ms, Memory: 88216 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,18 +1,15 @@
 import sys, heapq
 input = sys.stdin.readline
 
-n = int(input())
-task = sorted([list(map(int,input().split())) for _ in range(n)], key=lambda x:(x[0], x[1]))
-room = []
-heapq.heappush(room, 1)
-
-for st, ed in task:
-    
-    if room[0] <= st:
-        heapq.heappop(room)
-        heapq.heappush(room, ed)
-    else:
-        # heapq.heappush(room, ed_time)
-        heapq.heappush(room, ed)
-
-print(len(room))
+def solve():
+    n = int(input())
+    task = sorted([list(map(int,input().split())) for _ in range(n)], key=lambda x:(x[0], x[1]))
+    room = [0]
+    for st, ed in task:
+        
+        if room[0] <= st:
+            heapq.heapreplace(room, ed)
+        else:
+            heapq.heappush(room, ed)
+    return len(room)
+print(solve())
