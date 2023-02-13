9ae96b68309e1e11a145e1b1afb09d6e28433e32
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-13 15:00:51 +0900

    [Gold V] Title: 용액, Time: 180 ms, Memory: 47872 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,3 +1,4 @@
+import heapq
 n = int(input())
 nums = list(map(int,input().split()))
 
@@ -7,17 +8,13 @@ res = []
 while start < end:
     diff = nums[end]+nums[start]
     if diff>0:
-        res.append((diff, nums[start], nums[end]))
-        #heapq.heappush(res, (diff, nums[start], nums[end]))
+        heapq.heappush(res, (diff, nums[start], nums[end]))
         end -= 1
     elif diff<0:
-        res.append((-diff, nums[start], nums[end]))
-        #heapq.heappush(res, (-diff, nums[start], nums[end]))
+        heapq.heappush(res, (-diff, nums[start], nums[end]))
         start += 1
     else:
-        res.append((diff, nums[start], nums[end]))
-        #heapq.heappush(res, (diff, nums[start], nums[end]))
+        heapq.heappush(res, (diff, nums[start], nums[end]))
         break
-res.sort()
-dist,a,b = res.pop(0)
+ans, a, b= heapq.heappop(res)
 print(a,b)
