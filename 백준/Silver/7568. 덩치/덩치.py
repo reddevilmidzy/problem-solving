a1cf2bdbee5e9a55e17405d9084b1aba6c78b600
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-15 14:15:23 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -3,16 +3,19 @@ input = sys.stdin.readline
 body = []
 n = int(input().rstrip())
 for i in range(n):
-    body.append([*map(int, input().rstrip().split()), i])
-# print(body)
+    weight, height = map(int, input().rstrip().split())
+    a = [weight, height, i]
+    body.append(a)
 body.sort()
+rank = 0
 result = []
 for j in range(n-1):
-    rank = 0
     for k in range(j+1, n):
         if body[j][0] < body[k][0] and body[j][1] < body[k][1]:
             rank += 1
     result.append([body[j][2], rank+1])
+    rank = 0
 result.append([body[-1][2], 1])
-for l in sorted(result):
+result.sort()
+for l in result:
     print(l[1], end=" ")
