f1bbd833e1d87d0d904d816405016219619455d5
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-26 23:12:18 +0900

    [Gold IV] Title: 미로만들기, Time: 52 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,37 +1,29 @@
-from collections import deque
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
 dx = [0,1,-1,0]
 dy = [1,0,0,-1]
 
 def bfs():
-    queue = deque()
-    queue.append([0,0,0])
-    visited = [[[False]*n for _ in range(n)] for __ in range(2*n+1)]
-    visited[0][0][0] = True
-    while queue:
-        x,y,cnt = queue.popleft()
+    q = []
+    heapq.heappush(q, [0,0,0])
+    visited = [[n*n]*n for _ in range(n)]
+    while q:
+        cnt, x, y = heapq.heappop(q)
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
 
             if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
-            
-            if board[ny][nx] and not visited[cnt][ny][nx]:
-                queue.append([nx, ny, cnt])
-                visited[cnt][ny][nx] = True
-            elif not board[ny][nx] and not visited[cnt+1][ny][nx]: # 벽부숨
-                if cnt + 1 >= 2*n:
-                    continue
+            if board[ny][nx] and visited[ny][nx] > cnt:
+                visited[ny][nx] = cnt
+                heapq.heappush(q, [cnt, nx, ny])
+            if not board[ny][nx] and visited[ny][nx] > cnt + 1:
+                visited[ny][nx] = cnt + 1
+                heapq.heappush(q, [cnt+1, nx, ny])
 
-                queue.append([nx,ny,cnt+1])
-                visited[cnt+1][ny][nx] = True
-
-    for room in range(2*n):
-        if visited[room][n-1][n-1]:
-            return room
+    return visited[n-1][n-1]
 
 n = int(input())
 board = [list(map(int,input().rstrip())) for _ in range(n)]
