a5966c7c277a50ab69f5c8b95d9edb420c06fba1
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-27 21:40:22 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 432 ms, Memory: 234312 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
@@ -3,19 +3,18 @@ nums = list(map(int,input().split()))
 
 binary = [num%2 for num in nums]
 pre_fix = [0]*(n+1)
-dp = [0]*(1+n)
 
 for idx, val in enumerate(binary, 1):
     pre_fix[idx] = val + pre_fix[idx-1]
 
 st,ed = 0, 1
-ans = []
+ans = 0
 
 while st <= ed and ed <= n:
     tmp = pre_fix[ed] - pre_fix[st]
     if tmp <= k:
-        ans.append(ed - st - tmp)
+        ans = max(ans, ed - st - tmp)
         ed += 1
     else:
         st += 1
-print(max(ans))
+print(ans)
