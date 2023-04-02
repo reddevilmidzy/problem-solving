bbe807394afd0f8a0a563f007e9024a0e7134945
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-02 16:23:46 +0900

    [Silver I] Title: 곱셈, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -3,9 +3,7 @@ def solve(a,b,c):
         return a%c
     else:
         k = solve(a, b//2, c)
-        if b%2 == 0:
-            return k*k%c
-        else:
-            return k*k*a%c
+        if b%2 == 0: return k*k%c
+        else: return k*k*a%c
 a,b,c = map(int, input().split())
 print(solve(a,b,c))
