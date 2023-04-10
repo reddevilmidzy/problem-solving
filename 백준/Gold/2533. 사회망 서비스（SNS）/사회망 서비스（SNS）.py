9045f35dfdb5b74b6d29150818654c9d1273cf5b
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-10 15:07:53 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2040 ms, Memory: 277204 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tree = [[] for _ in range(n)]
+for _ in range(n-1):
+    u,v = map(int,input().split())
+    tree[u-1].append(v-1)
+    tree[v-1].append(u-1)
+
+leaf = [i for i in range(n) if len(tree[i]) == 1]
+visited = [0]*n
+while leaf:
+    node = leaf.pop()
+    for par in tree[node]:
+        for gra_par in tree[par]:
+            tree[gra_par].remove(par)
+            if len(tree[gra_par]) == 1:
+                leaf.append(gra_par)
+        visited[par] = 1
+        tree[par].clear()
+
+print(sum(visited))
