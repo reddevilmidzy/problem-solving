cc2b17ee14f44b0a60b1df58dfb61fd75b2b5436
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-13 15:33:01 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 47204 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,12 +1,12 @@
 n = int(input())
-road = list(map(int, input().split()))
-coin = list(map(int, input().split()))
-result = 0
-cheap = coin[0]
+dist = list(map(int,input().split()))
+money = list(map(int,input().split()))
+
+ans = 0
+won = money[0]
+
 for i in range(n-1):
-    result += road[i]*cheap
-    if cheap <= coin[i+1]:
-        pass
-    else:
-        cheap = coin[i+1]
-print(result)
+    ans += dist[i] * won
+    if won > money[i+1]:
+        won = money[i+1]
+print(ans)
