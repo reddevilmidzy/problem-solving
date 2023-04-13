d10ed3dff6d1970abf9ed754ed938ff3e0336cdf
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-13 17:07:31 +0900

    [Platinum V] Title: 할로윈 묘지, Time: 2972 ms, Memory: 132672 KB -BaekjoonHub

--- 백준/Platinum/3860. 할로윈 묘지/할로윈 묘지.py
+++ 백준/Platinum/3860. 할로윈 묘지/할로윈 묘지.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 import sys
-
 input = sys.stdin.readline
 
 dx = [1,0,-1,0]
@@ -8,6 +6,7 @@ dy = [0,1,0,-1]
 INF = int(1e9)
 
 def walk(y:int,x:int) -> None:
+    res = []
     for i in range(4):
         ny,nx = dy[i] + y, dx[i] + x
 
@@ -16,17 +15,18 @@ def walk(y:int,x:int) -> None:
             continue
 
         if board[ny][nx] != -1: # 무덤이 아니라면
-            graph[(y,x)].append((ny,nx,1))
+            res.append((ny,nx,1))
+    graph[(y,x)] = res
 
 def loop(y:int, x:int) -> None:
-    graph[(y,x)].append(hole[(y,x)])
-
+    graph[(y,x)] = [(hole[(y,x)])]
 
 def jump(y:int, x:int) -> None:
     if (y,x) in hole: # 묘비 입구라면
-        graph[(y,x)].append(hole[(y,x)])
+        graph[(y,x)] = [(hole[(y,x)])]
         return
     
+    res = []
     for i in range(4):
         ny,nx = dy[i] + y, dx[i] + x
 
@@ -35,8 +35,9 @@ def jump(y:int, x:int) -> None:
             continue
 
         if board[ny][nx] != -1: # 무덤이 아니라면
-            graph[(y,x)].append((ny,nx,1))
-
+            res.append((ny,nx,1))
+    graph[(y,x)] = res
+    
 def bellman(cur_y, cur_x):
     dist = [[INF]*w for _ in range(h)]
     dist[cur_y][cur_x] = 0
@@ -44,9 +45,7 @@ def bellman(cur_y, cur_x):
     for _ in range(h*w - 1):
         for i in range(h):
             for j in range(w):
-                if i == h-1 and j == w-1:
-                    continue
-                if dist[i][j] == INF:
+                if dist[i][j] == INF or board[i][j] == -1:
                     continue
                 for ny, nx, cost in graph[(i,j)]:
                     if dist[ny][nx] > dist[i][j] + cost:
@@ -54,11 +53,7 @@ def bellman(cur_y, cur_x):
 
     for i in range(h):
         for j in range(w):
-            if i == h-1 and j == w-1:
-                continue
-            if dist[i][j] == INF:
-                continue
-            if board[i][j] == -1: # 이건 사실 필요 없을듯
+            if dist[i][j] == INF or board[i][j] == -1:
                 continue
             for ny,nx,cost in graph[(i,j)]:
                 if dist[ny][nx] > dist[i][j] + cost:
@@ -67,25 +62,21 @@ def bellman(cur_y, cur_x):
     return dist[h-1][w-1] if dist[h-1][w-1] != INF else "Impossible"
 
 while True:
-
     # 너비, 높이
     w,h = map(int,input().split())
     if w == 0:
         break
-    # 0: 잔디, -1: 묘비, 1 ~ e : 구멍
+    # 0: 잔디, -1: 묘비, 2 : 구멍
     board = [[0]*w for _ in range(h)]
-    graph = defaultdict(list)
+    graph = dict()
     # 묘비 위치
     g = int(input())
-
     for _ in range(g):
         x,y = map(int,input().split())
         board[y][x] = -1 # 묘비
 
     e = int(input())
-
-    # key: 귀신 구멍 번호, val: 위치, 시간
-    hole = dict() # 굳이 딕트로 하지 않고, 리스트로 해도 될듯.
+    hole = dict()
     for i in range(e):
         x1,y1,x2,y2,t = map(int,input().split())
         if x1 == x2 and y1 == y2: # 둘이 같다면
@@ -106,4 +97,5 @@ while True:
                 loop(i,j)
             elif board[i][j] != -1: # 묘비가 아니라면, => 귀신 구멍
                 jump(i,j)
+    graph[(h-1, w-1)] = []
     print(bellman(0,0))
