ee1990d101b3a25274476ba9e3d214331dcd1249
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-14 16:15:13 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 1992 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    left, right = 0, n-1
+    res = sys.maxsize
+
+    for left in range(n-2):
+        for right in range(n-1,left+2,-1):
+            res = min(res, two_point(nums[left]+nums[right], left, right))
+            if not res:
+                return res
+    return res
+
+def two_point(elsa: int, left: int, right: int) -> int:
+    st, ed = left + 1, right - 1
+    res = sys.maxsize
+
+    while st <= ed:
+        anna = nums[st] + nums[ed]
+        res = min(res, abs(anna - elsa))
+        if anna < elsa:
+            st += 1
+        elif anna > elsa:
+            ed -= 1
+        else:
+            break
+    return res
+    
+
+n = int(input())
+nums = sorted(list(map(int,input().split())))
+
+print(solve())
