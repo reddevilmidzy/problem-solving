947310a0f2b3affd28b5188dd9f9e19865a5803c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-15 10:59:35 +0900

    [Gold V] Title: 합분해, Time: 256 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,10 +1,9 @@
+import sys
+input = sys.stdin.readline
 n,k = map(int,input().split())
 mod = int(1e9)
-
 dp = [[0]*(n+1) for _ in range(k)]
-
 dp[0] = [1]*(n+1)
-
 for i in range(1, k):
     for j in range(n+1):
         dp[i][j] = sum([dp[i-1][u] for u in range(j+1)])%mod
