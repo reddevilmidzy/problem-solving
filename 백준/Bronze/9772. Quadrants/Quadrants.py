d3addc4dbf2c0b20439da1347ad37d1d88c67dec
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-19 16:44:02 +0900

    [Bronze IV] Title: Quadrants, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9772. Quadrants/Quadrants.py
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -1,19 +1,20 @@
 import sys
 input = sys.stdin.readline
 
+def solve(x:int, y:int) -> str: 
+    if x > 0 and y > 0:
+        return "Q1"
+    elif x < 0 and y > 0:
+        return "Q2"
+    elif x < 0 and y < 0:
+        return "Q3"
+    elif x > 0 and y < 0:
+        return "Q4"
+    else:
+        return "AXIS"    
+
 while True:
     x,y = map(float,input().split())
+    print(solve(x,y))
     if x == 0 and y == 0:
-        print("AXIS")
         break
-    elif x > 0 and y > 0:
-        print("Q1")
-    elif x < 0 and y > 0:
-        print("Q2")
-    elif x < 0 and y < 0:
-        print("Q3")
-    elif x > 0 and y < 0:
-        print("Q4")
-    else:
-        print("AXIS")
-    
