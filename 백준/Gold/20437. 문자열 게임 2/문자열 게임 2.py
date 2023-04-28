d9842c38f0e4afe05e1b8db84d1066d48d761735
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-04-28 21:27:38 +0900

    [Gold V] Title: 문자열 게임 2, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    w = input().rstrip()
+    k = int(input())
+    n = len(w)
+    word = dict()
+    for i in range(97, 123):
+        word[chr(i)] = []
+    for i in range(n):
+        word[w[i]].append(i)
+    
+    res = []
+
+    for val in word.values():
+        if len(val) < k:
+            continue
+        for i in range(len(val)-k+1):
+            res.append(val[i+k-1] - val[i]+1)
+    print(f"{min(res)} {max(res)}" if res else -1)
