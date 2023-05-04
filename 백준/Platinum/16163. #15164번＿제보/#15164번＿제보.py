68c4817c5d1cc69452b5178ea83f58e9f94a16f0
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-05-04 17:27:08 +0900

    [Platinum V] Title: #15164번_제보, Time: 424 ms, Memory: 218644 KB -BaekjoonHub

--- 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.py
+++ 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.py
@@ -1,19 +1,13 @@
-def manachers(s:str, n:int) -> list[int]:
+def manachers(s,n):
     r,p = 0,0
     arr = [0]*n
     for i in range(n):
-        if i <= r:
-            arr[i] = min(arr[2*p-i], r-i)
-        else:
-            arr[i] = 0
-            
+        arr[i] = 0 if i>r else min(arr[2*p-i], r-i)            
         while i-arr[i]-1 >= 0 and i+arr[i]+1 < n and s[i-arr[i]-1] == s[i+arr[i]+1]:
             arr[i] += 1
-
         if r < i + arr[i]:
             r = i + arr[i]
             p = i
-    return arr
+    return sum([(i+1)//2 if i else 0 for i in arr])
 s = "#"+"#".join(input())+"#"
-ans = sum([(i+1)//2 if i else 0 for i in manachers(s, len(s))])
-print(ans)
+print(manachers(s, len(s)))
