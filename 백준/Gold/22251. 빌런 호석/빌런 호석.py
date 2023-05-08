dd32a702383cbf2859d586da63b44ce5e071cefd
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-05-08 19:06:26 +0900

    [Gold V] Title: 빌런 호석, Time: 520 ms, Memory: 117420 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.py
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.py
@@ -3,42 +3,44 @@
 # 1 ~ p 개를 반전
 # 현재 X층에 있음
 
-ans = []
+ans = 0
+
+board = [[0, 4, 3, 3, 4, 3, 2, 3, 1, 2],
+        [4, 0, 5, 3, 2, 5, 6, 1, 5, 4],
+        [3, 5, 0, 2, 5, 4, 3, 4, 2, 3],
+        [3, 3, 2, 0, 3, 2, 3, 2, 2, 1],
+        [4, 2, 5, 3, 0, 3, 4, 3, 3, 2],
+        [3, 5, 4, 2, 3, 0, 1, 4, 2, 1],
+        [2, 6, 3, 3, 4, 1, 0, 5, 1, 2],
+        [3, 1, 4, 2, 3, 4, 5, 0, 4, 3],
+        [1, 5, 2, 2, 3, 2, 1, 4, 0, 1],
+        [2, 4, 3, 1, 2, 1, 2, 3, 1, 0]]
+
 
 def bt(s:list[int], p:int, val: list[int]):
     if len(s) == k:
         tmp = 0
         for i,j in enumerate(s):
             tmp += val[i][j]
+        global ans
         if 1 <= tmp <= p and 1 <= int("".join(map(str,s))) <= n:
-            ans.append(s[::])
+            ans += 1
         return 
     for i in range(10):
         s.append(i)
         bt(s, p, val)
         s.pop()
 
-def diff(a: int, b: int) -> int:
-    a = disit[a]
-    b = disit[b]
-    res = 0
-    for i,j in zip(a,b):
-        if i != j:
-            res += 1
-    return res
-
 def solve(nums: list[int], p: int):
     res = [[] for _ in range(len(nums))]
     idx = 0
     for i in nums:
         for j in range(10):
-            res[idx].append((diff(i,j)))
+            res[idx].append(board[i][j])
         idx += 1
     return res
-
 n,k,p,x = map(int,input().split())
 nums = list(map(int, str(x).zfill(k)))
-disit = ["1110111", "0010010", "1011101", "1011011", "0111010", "1101011", "1101111", "1010010", "1111111", "1111011"]
 res = solve(nums, p)
 bt([], p, res)
-print(len(ans))
+print(ans)
