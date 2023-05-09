bf1c7ba22dcbd6bd74cf7327f8f3e267a6e85156
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-05-09 19:28:56 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 228 ms, Memory: 44104 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -2,13 +2,14 @@ from bisect import bisect_left
 import sys
 input = sys.stdin.readline
 
+def solve():
+    for _ in range(m):
+        print(name[bisect_left(level, int(input()))])
 n,m = map(int,input().split())
 name = []
 level = []
 for _ in range(n):
-    a,b = map(str,input().split())
+    a,b = input().split()
     name.append(a)
     level.append(int(b))
-
-for _ in range(m):
-    print(name[bisect_left(level, int(input()))])
+solve()
