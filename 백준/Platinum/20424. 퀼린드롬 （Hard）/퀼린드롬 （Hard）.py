2e897ba35e7f0820afd723dceb95ab522e89eb50
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-06-05 22:35:24 +0900

    [Platinum II] Title: 퀼린드롬 (Hard), Time: 1428 ms, Memory: 308336 KB -BaekjoonHub

---
+++ 백준/Platinum/20424. 퀼린드롬 （Hard）/퀼린드롬 （Hard）.py
@@ -0,0 +1,92 @@
+def is_same(left:str, right:str) -> bool:
+    return left.lower() == q_dict[right.lower()] or left.lower() == q_dict[right.upper()] or left.upper() == q_dict[right.lower()] or left.upper() == q_dict[right.upper()]
+
+def can_be_palin(string: str):
+    for c in string:
+        if q_dict[c.lower()] == "" and q_dict[c.upper()] == "":
+            return False
+    return True
+
+def get_palin(val:str, center: str) -> str:
+    left = []
+    right = []
+    cen = []
+    for c in center:
+        if q_dict[c.lower()] == "":
+            cen.append(c.upper())
+        else:
+            cen.append(c.lower())
+
+    for c in val:
+        if q_dict[c.lower()] != "":
+            left.append(c.lower())
+            right.append(q_dict[c.lower()])
+        else:
+            left.append(c.upper())
+            right.append(q_dict[c.upper()])
+    
+    return "".join(left) + "".join(cen) + "".join(right[::-1])
+
+def manacher(s:str, n:int):
+    r,p = 0,0
+    arr = [0]*n
+    for i in range(n):
+        if i <= r:
+            arr[i] = min(arr[2*p-i], r-i)
+        else:
+            arr[i] = 0
+        while i-arr[i]-1>=0 and i+arr[i]+1 < n and is_same(s[i-arr[i]-1], s[i+arr[i]+1]):
+            arr[i] += 1
+        if r<i+arr[i]:
+            r = i + arr[i]
+            p = i
+    return arr
+
+q_dict = dict()
+my_self = ["A", "H", "I", "M", "O", "T", "U", "V", "W", "X", "Y", 
+           "i", "l", "m", "n", "o", "u", "v", "w", "x", 
+           "0", "1", "8",
+           "#"]
+
+couple = [("E", "3"), ("S", "2"), ("Z", "5"), ("b", "d"), ("p", "q"), ("r", "7")]
+
+for i in range(65, 91):
+    q_dict[chr(i)] = "" # 대문자
+    q_dict[chr(i).lower()] = "" # 소문자
+for i in range(10):
+    q_dict[str(i)] = ""
+for c in my_self:
+    q_dict[c] = c
+for b,d in couple:
+    q_dict[b] = d
+    q_dict[d] = b
+
+origin = input()
+if not can_be_palin(origin):
+    print(-1)
+    exit()
+
+s = "#" + "#".join(origin) + "#"
+n = len(s)
+radius = manacher(s, n)
+arr = manacher(s ,n)
+
+result = []
+
+for idx,val in enumerate(arr):
+    if idx + val == n-1:
+        idx = (idx-1)//2 
+        val = (n-1)//2-val
+        result.append(get_palin(origin[:val], origin[val:]))
+        break
+
+# 왼쪽에 새로운 문자 추가
+origin = origin[::-1]
+for idx, val in enumerate(arr[::-1]):
+    if idx + val == n-1:
+        idx = (idx-1)//2
+        val = (n-1)//2-val
+        result.append(get_palin(origin[:val], origin[val:])[::-1])
+        break
+
+print(sorted(result, key=lambda x:len(x))[0])
