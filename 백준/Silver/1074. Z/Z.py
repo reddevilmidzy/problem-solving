704f7a01da6b873a2db86934ef50b8ff4cce3ba7
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-06-27 15:18:29 +0900

    [Silver I] Title: Z, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,26 +1,20 @@
 n,r,c= map(int,input().split()) # r 세로
-
 check = 2**(n-1)
 ans = 0
 plus = 4**(n-1)
 while n:
-    #print('n',n,'r',r,'c',c,'check',check)
     if r < check and c < check:
         ans += plus*0
-        #print('행보',0)
     elif r < check and c >= check:
         ans += plus*1
         c -= check
-        #print('행보',1)
     elif r >= check and c < check:
         ans += plus*2
         r -= check
-        #print('행보',2)
     elif r >= check and c >= check:
         ans += plus*3
         r -= check
         c -= check
-        #print('행보',3)
     n -= 1
     check = 2**(n-1)
     plus = 4**(n-1)
