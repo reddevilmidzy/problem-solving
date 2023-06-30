95b7894d35c12af7e4bd857a3c75768f5053761c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-01 00:07:08 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -0,0 +1,19 @@
+n = int(input())
+nums = list(map(int,input().split()))
+dp = [1]*n
+route = [i for i in range(n)]
+
+for i in range(n):
+    for j in range(i,-1,-1):
+        if nums[j] < nums[i] and dp[i] < dp[j] + 1:
+            dp[i] = dp[j] + 1
+            route[i] = j
+
+node = dp.index(max(dp))
+ans = [nums[node]]
+
+while node != route[node]:
+    node = route[node]
+    ans.append(nums[node])
+print(max(dp))
+print(*ans[::-1])
