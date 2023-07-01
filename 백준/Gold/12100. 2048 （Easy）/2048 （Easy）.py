0d82062d01fd082092a7934b416118f3e0472b45
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-02 00:49:48 +0900

    [Gold II] Title: 2048 (Easy), Time: 132 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -2,38 +2,34 @@ from sys import stdin
 input = stdin.readline
 
 def solve(move:int, board: list[list[int]]) -> list[list[int]]:
+    res = [[0]*n for _ in range(n)]
     if move == 0:
-        res = []
         for i in range(n):
-            tmp = [0]*n
             idx = 0
             tmp_board = [board[i][j] for j in range(n) if board[i][j]]
             m = len(tmp_board)
             j = m-1
             while j > 0:
                 if tmp_board[j] == tmp_board[j-1]:
-                    tmp[idx] = tmp_board[j]*2
+                    res[i][n-idx-1] = tmp_board[j]*2
                     idx += 1
                     j -= 2
                     if j == 0:
-                        tmp[idx] = tmp_board[j]
+                        res[i][n-idx-1] = tmp_board[j]
                         idx += 1
                 else:
-                    tmp[idx] = tmp_board[j]
+                    res[i][n-idx-1] = tmp_board[j]
                     idx += 1
                     j -= 1
                     if j == 0:
-                        tmp[idx] = tmp_board[j]
+                        res[i][n-idx-1] = tmp_board[j]
                         idx += 1
             if m == 1:
-                tmp[idx] = tmp_board[0]
+                res[i][n-idx-1] = tmp_board[0]
 
-            res.append(tmp[::-1])
 
     elif move == 1:
-        res = []
         for i in range(n):
-            tmp = [0]*n
             idx = 0
             tmp_board = [board[i][j] for j in range(n) if board[i][j]]
             j = 0
@@ -41,82 +37,73 @@ def solve(move:int, board: list[list[int]]) -> list[list[int]]:
 
             while j < m-1:
                 if tmp_board[j] == tmp_board[j+1]:
-                    tmp[idx] = tmp_board[j]*2
+                    res[i][idx] = tmp_board[j]*2
                     idx += 1
                     j += 2
                     if j == m-1:
-                        tmp[idx] = tmp_board[j]
+                        res[i][idx] = tmp_board[j]
                         idx += 1
                 else:
-                    tmp[idx] = tmp_board[j]
+                    res[i][idx] = tmp_board[j]
                     idx += 1
                     j += 1
                     if j == m-1:
-                        tmp[idx] = tmp_board[j]
+                        res[i][idx] = tmp_board[j]
                         idx +=  1
             
             if m == 1:
-                tmp[idx] = tmp_board[0]
+                res[i][idx] = tmp_board[0]
 
-            res.append(tmp[:])
 
     elif move == 2:
-        res = [[0]*n for _ in range(n)]
         for i in range(n):
-            tmp = [0]*n
             idx = 0
             tmp_board = [board[j][i] for j in range(n) if board[j][i]]
             m = len(tmp_board)
             j = m - 1
             while j > 0:
                 if tmp_board[j] == tmp_board[j-1]:
-                    tmp[idx] = tmp_board[j]*2
+                    res[n-idx-1][i] = tmp_board[j]*2
                     idx += 1
                     j -= 2
                     if j == 0:
-                        tmp[idx] = tmp_board[j]
+                        res[n-idx-1][i] = tmp_board[j]
                         idx += 1
                 else:
-                    tmp[idx] = tmp_board[j]
+                    res[n-idx-1][i] = tmp_board[j]
                     idx += 1
                     j -= 1
                     if j == 0:
-                        tmp[idx] = tmp_board[j]
+                        res[n-idx-1][i] = tmp_board[j]
                         idx += 1
             if m == 1:
-                tmp[idx] = tmp_board[0]
+                res[n-idx-1][i] = tmp_board[0]
 
-            for k in range(n):
-                res[n - k - 1][i] = tmp[k]
     else:
-        res = [[0]*n for _ in range(n)]
         for i in range(n):
-            tmp = [0]*n
             idx = 0
             tmp_board = [board[j][i] for j in range(n) if board[j][i]]
             m = len(tmp_board)
             j = 0
             while j < m - 1:
                 if tmp_board[j] == tmp_board[j+1]:
-                    tmp[idx] = tmp_board[j]*2
+                    res[idx][i] = tmp_board[j]*2
                     idx += 1
                     j += 2
                     if j == m-1:
-                        tmp[idx] = tmp_board[j]
+                        res[idx][i] = tmp_board[j]
                         idx += 1
                 else:
-                    tmp[idx] = tmp_board[j]
+                    res[idx][i] = tmp_board[j]
                     idx += 1
-                    tmp.append(tmp_board[j])
                     j += 1
                     if j == m-1:
-                        tmp[idx] = tmp_board[j]
+                        res[idx][i] = tmp_board[j]
                         idx += 1
 
             if m == 1:
-                tmp[idx] = tmp_board[0]
-            for k in range(n):
-                res[k][i] = tmp[k]
+                res[idx][i] = tmp_board[0]
+
 
     if res == board:
         return []
@@ -132,6 +119,7 @@ def dfs(depth: int, board: list[list[int]]) -> None:
         new = solve(move, board)
         if new:
             dfs(depth+1, new)
+
 
 n = int(input())
 board = [list(map(int,input().split())) for _ in range(n)]
