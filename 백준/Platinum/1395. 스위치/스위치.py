41b43f9b1c9f71e98238aecaf1fcd044b8af6740
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-05 23:13:44 +0900

    [Platinum III] Title: 스위치, Time: 1428 ms, Memory: 138128 KB -BaekjoonHub

---
+++ 백준/Platinum/1395. 스위치/스위치.py
@@ -0,0 +1,48 @@
+import sys, math
+input = sys.stdin.readline
+
+def update_lazy(tree,lazy,node,start,end):
+    if lazy[node] != 0: # lazy하게 나중에 하겠다고 한게 있다면
+        tree[node] = (end-start+1) - tree[node]
+        if start != end:
+            lazy[node*2] ^= lazy[node]
+            lazy[node*2+1] ^= lazy[node]
+        lazy[node] = 0 # 다 더했으니 지워주자
+
+def update_range(tree, lazy, node, start, end, left, right, diff):
+    update_lazy(tree,lazy,node,start,end)
+    if left > end or right < start:
+        return 
+    if left <= start and end <= right:
+        tree[node] = (end-start+1) - tree[node]
+        if start != end:
+            lazy[node*2] ^= diff
+            lazy[node*2+1] ^= diff
+        return
+
+    update_range(tree,lazy,node*2,start,(start+end)//2,left,right,diff)
+    update_range(tree,lazy,node*2+1,(start+end)//2+1,end,left,right,diff)
+    tree[node] = tree[node*2] + tree[node*2+1]
+
+def query(tree,lazy,node,start,end,left,right):
+    update_lazy(tree,lazy,node,start,end)
+    if left>end or right<start:
+        return 0
+    if left<= start and end <= right:
+        return tree[node]
+    lsum = query(tree,lazy,node*2,start,(start+end)//2,left,right)
+    rsum = query(tree,lazy,node*2+1, (start+end)//2+1, end,left,right)
+    return lsum + rsum
+
+n,m = map(int,input().split())
+h = math.ceil(math.log2(n))
+tree_size = 1<<(h+1)
+tree = [0]*tree_size
+lazy = [0]*tree_size
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    if a == 0:
+        update_range(tree,lazy,1,0,n-1,b-1,c-1,1)
+    else:
+        print(query(tree,lazy,1,0,n-1,b-1,c-1))
