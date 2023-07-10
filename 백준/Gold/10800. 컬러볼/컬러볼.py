4082483bc622dedaf5ad40e5055ae170f941accb
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-10 19:03:17 +0900

    [Gold II] Title: 컬러볼, Time: 1400 ms, Memory: 119156 KB -BaekjoonHub

---
+++ 백준/Gold/10800. 컬러볼/컬러볼.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+balls = []
+color = defaultdict(int)
+size = defaultdict(int)
+
+for i in range(n):
+    c,s = map(int,input().split())
+    balls.append([i, c, s, 0, 0])
+
+balls.sort(key=lambda x:(x[2], x[1]))
+
+for i in range(n):
+    balls[i][3] = balls[i-1][3] + balls[i][2]
+    color[balls[i][1]] += balls[i][2]
+    size[balls[i][2]] += balls[i][2]
+
+    balls[i][4] = balls[i][3] - color[balls[i][1]] - size[balls[i][2]] + balls[i][2]
+    if balls[i][2] == balls[i-1][2] and balls[i][1] == balls[i-1][1]:
+        balls[i][4] = balls[i-1][4]
+
+balls.sort()
+print(*[ball[4] for ball in balls], sep='\n')
