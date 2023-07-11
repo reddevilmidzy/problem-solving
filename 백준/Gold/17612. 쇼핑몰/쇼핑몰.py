cb1f042a2f8ffe751c2243d1fc1d045f3db5bfdf
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-11 17:10:56 +0900

    [Gold II] Title: 쇼핑몰, Time: 400 ms, Memory: 75524 KB -BaekjoonHub

---
+++ 백준/Gold/17612. 쇼핑몰/쇼핑몰.py
@@ -0,0 +1,22 @@
+from heapq import heappush, heappop, heapify
+from sys import stdin
+input = stdin.readline
+
+n,k = map(int,input().split())
+nums = [list(map(int,input().split())) for _ in range(n)]
+
+# k 개의 카운터
+counter = [(0,i) for i in range(k)]
+heapify(counter)
+
+time = [0]*k
+done = []
+
+for i in range(n):
+    # 가장 빨리 작업 끝나는 카운터
+    t,x = heappop(counter)
+    time[x] += nums[i][1] # 시간 추가
+    heappush(counter, (time[x], x))
+    done.append((time[x], -x, i))
+
+print(sum(nums[val[2]][0]*i for i,val in enumerate(sorted(done), 1)))
