24a2fa48960498a9c58df57c5a3811d0fd7acfa2
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-19 15:26:43 +0900

    [Bronze III] Title: Ratownik, Time: 148 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/8574. Ratownik/Ratownik.py
+++ 백준/Bronze/8574. Ratownik/Ratownik.py
@@ -2,9 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n,k,y,x = map(int,input().split())
+k *= k
 ans = 0
 
 for _ in range(n):
     r,c = map(int,input().split())
-    ans += ((y-r)**2 + (x-c)**2)**0.5 > k
+    ans += ((y-r)**2 + (x-c)**2) > k
 print(ans)
