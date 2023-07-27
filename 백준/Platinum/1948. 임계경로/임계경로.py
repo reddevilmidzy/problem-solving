21fe29e69f2b30bb63582d847fc458459360b8e7
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-07-28 08:09:00 +0900

    [Platinum V] Title: 임계경로, Time: 276 ms, Memory: 52888 KB -BaekjoonHub

---
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -0,0 +1,54 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+back = [[] for _ in range(n+1)]
+
+cnt = [0]*(n+1)
+
+for _ in range(m):
+    u,v,w = map(int,input().split())
+    graph[u].append((v,w))
+    back[v].append((u,w))
+    cnt[v] += 1
+
+
+st,ed = map(int,input().split())
+# print(cnt)
+time = [0]*(n+1)
+queue = deque()
+queue.append(st)
+
+for _ in range(n):
+    node = queue.popleft()
+
+    for nxt,wth in graph[node]:
+        cnt[nxt] -= 1
+        
+        if time[nxt] < time[node] + wth:
+            time[nxt] = time[node] + wth
+
+        if not cnt[nxt]:            
+            queue.append(nxt)
+
+queue.append(ed)
+
+edges = 0
+
+while queue:
+    node = queue.popleft()
+
+    for nxt,wgh in back[node]:
+        if cnt[nxt]:
+            edges += 1
+            continue
+        if time[node] - time[nxt] == wgh and not cnt[nxt]:
+            queue.append(nxt)
+            cnt[nxt] = 1
+            edges += 1
+
+print(time[ed])
+print(edges)
