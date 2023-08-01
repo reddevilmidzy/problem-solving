949c712a57149819debfbb72b741e23c7d0e5d5b
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-08-01 20:48:13 +0200

    [Gold III] Title: 욕심쟁이 판다, Time: 416 ms, Memory: 206920 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,29 +1,25 @@
-import sys
-sys.setrecursionlimit(10**5)
-input = sys.stdin.readline
+def dfs(y, x):
+    if dp[y][x] != -1: return dp[y][x]
+    else:
+        dp[y][x] = 1
+        for i in range(4):
+            ny = dy[i]+y
+            nx = dx[i]+x
+            if 0<=ny<n and 0<=nx<n:
+                if board[y][x] < board[ny][nx]:
+                    dp[y][x] = max(dp[y][x], dfs(ny,nx)+1)    
+        return dp[y][x]
 
-dy = (1,0,-1,0)
-dx = (0,1,0,-1)
-
-def dfs(y:int, x:int) -> int:
-    if dp[y][x]: return dp[y][x]
-
-    dp[y][x] = 1
-
-    for i in range(4):
-        ny,nx = dy[i]+y, dx[i]+x
-        if ny < 0 or ny >= n or nx < 0 or nx >= n: continue
-        if board[y][x] < board[ny][nx]:
-            dp[y][x] = max(dp[y][x], dfs(ny,nx)+1)
-    
-    return dp[y][x]
+dy = (-1,1,0,0)
+dx = (0,0,-1,1)
 
 n = int(input())
 board = [list(map(int,input().split())) for _ in range(n)]
-dp = [[0]*n for _ in range(n)]
+dp = [[-1]*n for _ in range(n)]
 
-for r in range(n):
-    for c in range(n):
-        dfs(r,c)
+for y in range(n):
+    for x in range(n):
+        if dp[y][x] == -1:
+            dfs(y,x)
 
 print(max(map(max, dp)))
