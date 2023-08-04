833e40886d54cfe7d1fd464baf0593fdce9f7546
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-08-04 11:55:51 +0200

    [Platinum V] Title: 이름 정하기, Time: 180 ms, Memory: 51008 KB -BaekjoonHub

---
+++ 백준/Platinum/16900. 이름 정하기/이름 정하기.py
@@ -0,0 +1,17 @@
+def kmp(pattern):
+    n = len(pattern)
+    pi = [0]*n
+    i = 0
+    for j in range(1, n):
+        while i > 0 and pattern[i] != pattern[j]:
+            i = pi[i-1]
+        if pattern[i] == pattern[j]:
+            i += 1
+            pi[j] = i
+    return pi
+
+word, k = input().split()
+pi = kmp(word)
+n = len(word)
+k = int(k)
+print(n + (n-pi[-1])*(k-1))
