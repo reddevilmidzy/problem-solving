aee6432482f9195cb321ae2f113fd39f5a47f1c6
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-08-24 22:53:01 +0900

    [Platinum IV] Title: 유럽여행, Time: 304 ms, Memory: 50492 KB -BaekjoonHub

--- 백준/Platinum/1185. 유럽여행/유럽여행.py
+++ 백준/Platinum/1185. 유럽여행/유럽여행.py
@@ -18,24 +18,27 @@ def union(parent:list[int], a:int, b:int) -> None:
     else:
         parent[a] = b
 
-n,p = map(int,input().split())
-parent = [i for i in range(n+1)]
-cost = [0] + [int(input()) for _ in range(n)]
-hq = []
+def solve():
+    n,p = map(int,input().split())
+    parent = [i for i in range(n+1)]
+    cost = [0] + [int(input()) for _ in range(n)]
+    hq = []
 
-for _ in range(p):
-    u,v,w = map(int,input().split())
-    heapq.heappush(hq, (2*w+cost[u]+cost[v], u,v))
+    for _ in range(p):
+        u,v,w = map(int,input().split())
+        heapq.heappush(hq, (2*w+cost[u]+cost[v], u,v))
 
-nodes = [0]*(n+1)
-ans = min(cost[1:])
-edges = n-1
+    nodes = [0]*(n+1)
+    ans = min(cost[1:])
+    edges = n-1
 
-while hq:
-    dist,u,v = heapq.heappop(hq)
-    if find(parent, u) != find(parent, v):
-        union(parent, u, v)
-        ans += dist
-        edges -= 1
-        if not edges: break
-print(ans)
+    while hq:
+        dist,u,v = heapq.heappop(hq)
+        if find(parent, u) != find(parent, v):
+            union(parent, u, v)
+            ans += dist
+            edges -= 1
+            if not edges: break
+    return ans
+if __name__ == "__main__":
+    print(solve())
