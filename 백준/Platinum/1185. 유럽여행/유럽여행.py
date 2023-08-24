78b2004e982d44fc238bad1560b75510e8479d3b
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-08-24 22:58:19 +0900

    [Platinum IV] Title: 유럽여행, Time: 272 ms, Memory: 60252 KB -BaekjoonHub

--- 백준/Platinum/1185. 유럽여행/유럽여행.py
+++ 백준/Platinum/1185. 유럽여행/유럽여행.py
@@ -1,6 +1,5 @@
 # 간선은 정확이 두번 사용
 # 정점은 두번이상
-
 import sys, heapq
 input = sys.stdin.readline
 
@@ -22,23 +21,17 @@ def solve():
     n,p = map(int,input().split())
     parent = [i for i in range(n+1)]
     cost = [0] + [int(input()) for _ in range(n)]
-    hq = []
-
-    for _ in range(p):
-        u,v,w = map(int,input().split())
-        heapq.heappush(hq, (2*w+cost[u]+cost[v], u,v))
-
-    nodes = [0]*(n+1)
+    edges = [list(map(int,input().split())) for _ in range(p)]
+    edges.sort(key=lambda x:2*x[2]+cost[x[0]]+cost[x[1]])
     ans = min(cost[1:])
-    edges = n-1
+    mst = 0
 
-    while hq:
-        dist,u,v = heapq.heappop(hq)
+    for u,v,w in edges:
         if find(parent, u) != find(parent, v):
             union(parent, u, v)
-            ans += dist
-            edges -= 1
-            if not edges: break
+            ans += 2*w+cost[u]+cost[v]
+            mst += 1
+            if mst == n-1: break
     return ans
 if __name__ == "__main__":
     print(solve())
