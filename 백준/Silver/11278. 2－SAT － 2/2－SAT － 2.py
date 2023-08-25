e9cb03995e89497e5cc9c7921d52928bc650a241
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-08-25 19:46:53 +0900

    [Silver I] Title: 2-SAT - 2, Time: 800 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11278. 2－SAT － 2/2－SAT － 2.py
@@ -0,0 +1,29 @@
+def bt(s:list[int], idx:int) -> None:
+    if idx > n:
+        for x,y in cnf:
+            if x < 0:
+                x = not s[abs(x)]
+            else:
+                x = s[abs(x)]
+            if y < 0:
+                y = not s[abs(y)]
+            else:
+                y = s[abs(y)]
+
+            if not (x | y):
+                break
+        else:
+            print(1)
+            print(*[+i for i in s[1:]])
+            exit()
+
+        return
+    for i in [False, True]:
+        s[idx] = i
+        bt(s, idx+1)
+        s[idx] = not i
+
+n,m = map(int,input().split())
+cnf = [list(map(int,input().split())) for _ in range(m)]
+bt([False]*(n+1), 1)
+print(0)
