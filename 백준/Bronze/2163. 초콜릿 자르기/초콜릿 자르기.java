c1d63b4c7e0f7cf91e552a7169a0efa6d467c373
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-09-04 01:07:38 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 196 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.next());
+        int m = Integer.parseInt(sc.next());
+
+        System.out.println(n*m-1);
+    }
+}
