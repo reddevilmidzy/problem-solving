9d21a6486c8d9c32f34ff7ca19f742d7b48cfe84
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-09-04 16:44:40 +0900

    [Platinum V] Title: 문자열과 쿼리, Time: 812 ms, Memory: 72072 KB -BaekjoonHub

---
+++ 백준/Platinum/13713. 문자열과 쿼리/문자열과 쿼리.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+def z_fun(s:str):
+    n = len(s)
+    z = [0]*n
+    z[0] = n
+    r = 0
+    l = 0
+    for i in range(1, n):
+        if i <= r:
+            z[i] = min(r-i+1, z[i-l])
+        while i+z[i] < n and s[i+z[i]] == s[z[i]]:
+            z[i] += 1
+        if r < i+z[i]-1:
+            r = i+z[i]-1
+            l = i
+    
+    return z
+
+z = z_fun(input().rstrip()[::-1])
+n = len(z)
+m = int(input())
+for _ in range(m):
+    print(z[n-int(input())])
