59a951e7e303b26a542edd33528901111a41490c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-09-14 00:05:36 +0900

    [Platinum V] Title: Big Macs Around the World, Time: 1496 ms, Memory: 119520 KB -BaekjoonHub

--- 백준/Platinum/5942. Big Macs Around the World/Big Macs Around the World.py
+++ 백준/Platinum/5942. Big Macs Around the World/Big Macs Around the World.py
@@ -4,21 +4,16 @@ INF = sys.maxsize
 
 def bellman(st:int, ed:int, val:int):
     dist = [INF for _ in range(n+1)]
-
     dist[st] = val
 
     for _ in range(n-1):
         for u,v,w in graph:
             if dist[u] != INF and dist[u] * w < dist[v]:
                 dist[v] = dist[u] * w
-    
-    if dist[ed] == INF:
-        return 0
 
     for u,v,w in graph:
-        if dist[u] != INF and dist[u] * w < dist[v]:
+        if dist[u] * w < dist[v]:
             return 0
-
     return dist[ed]
 
 n,m,val,st,ed = input().rstrip().split()
