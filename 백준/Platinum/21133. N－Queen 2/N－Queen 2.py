baf0fc54f9336fd18fdf11d2fc3cd8e7c295e4ef
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-09-28 23:16:52 +0900

    [Platinum II] Title: N-Queen 2, Time: 52 ms, Memory: 31388 KB, Score: 500 point -BaekjoonHub

--- 백준/Platinum/21133. N－Queen 2/N－Queen 2.py
+++ 백준/Platinum/21133. N－Queen 2/N－Queen 2.py
@@ -1,6 +1,23 @@
 n = int(input())
-
-for i in range(2, n+1, 2):
-    print(i)
-for i in range(1, n+1, 2):
-    print(i)
+t = n//2
+if n%6==2:
+    for i in range(1, t+1):
+        print(i*2)
+    print(3)
+    print(1)
+    for i in range(3, t):
+        print(i*2+1)
+    print(5)
+elif n%6==3:
+    for i in range(2, t+1):
+        print(i*2)
+    print(2)
+    for i in range(2, t+1):
+        print(i*2+1)
+    print(1)
+    print(3)
+else:
+    for i in range(2, n+1,2):
+        print(i)
+    for i in range(1, n+1, 2):
+        print(i)
