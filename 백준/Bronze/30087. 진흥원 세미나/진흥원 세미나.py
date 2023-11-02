8463b5fefb29543a042482dac3e68480fc918658
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2023-11-02 17:39:49 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,4 @@
+d={"Algorithm":204, "DataAnalysis":207, "ArtificialIntelligence":302, "CyberSecurity":"B101", "Network":303, "Startup":501, "TestStrategy":105}
+n=int(input())
+for _ in range(n):
+    print(d[input()])
