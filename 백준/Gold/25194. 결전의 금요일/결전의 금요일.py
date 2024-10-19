2cd6619693d1fc4c05b84e65935478e6e6e98452
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-10-20 01:35:45 +0900

    [Gold V] Title: 결전의 금요일, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/25194. 결전의 금요일/결전의 금요일.py
+++ 백준/Gold/25194. 결전의 금요일/결전의 금요일.py
@@ -1,17 +1,15 @@
 import sys
-input = sys.stdin.readline
-n = int(input())
-nums = list(map(int,input().split()))
-dp = [1]+[0]*6
+n=int(input())
+nums=list(map(int,input().split()))
+dp=[1]+[0]*6
 for i in range(n):
-    tmp = [0]*7
-    for j in range(7):        
+    tmp=[0]*7
+    for j in range(7):
         if dp[j]:
-            tmp[j] = 1
-            tmp[(j+nums[i]%7)%7] = 1
+            tmp[j]=1
+            tmp[(j+nums[i]%7)%7]=1
     if tmp[4]:
         print("YES")
-        break
-    dp = tmp
-else:
-    print("NO")
+        exit()
+    dp=tmp
+print("NO")
