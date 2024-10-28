0db1db1cfe62f7af50b9f1c968c0ab4afe101c04
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-10-28 10:59:41 +0900

    [Gold IV] Title: 부분합, Time: 132 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -7,15 +7,15 @@ pre_fix = [0]
 for num in nums:
     pre_fix.append(num+pre_fix[-1])
 st,ed = 0,1
-ans = 100001
+ans = []
 while ed <= n:
     tmp = pre_fix[ed] - pre_fix[st]
     if tmp == s:
-        ans = min(ans, ed-st)
+        ans.append(ed-st)
         ed += 1
     elif tmp < s:
         ed += 1
     else:
-        ans = min(ans, ed-st)
+        ans.append(ed-st)
         st += 1
-print(ans if ans != 100001 else 0)
+print(min(ans) if ans else 0)
