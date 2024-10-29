aaf5e36843ed0e6cac176543a78577e3125efe32
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-10-29 14:39:36 +0900

    [Gold II] Title: 대표 선수, Time: 1392 ms, Memory: 130764 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.py
+++ 백준/Gold/2461. 대표 선수/대표 선수.py
@@ -1,11 +1,10 @@
 import sys, heapq
 input = sys.stdin.readline
 
-INF = int(1e9) + 7
 n,m = map(int,input().split())
 nums = [sorted(list(map(int,input().split()))) for _ in range(n)]
 
-res = []
+res = int(1e9)
 pos = [0]*n
 
 max_val = max([nums[i][0] for i in range(n)])
@@ -14,10 +13,11 @@ heapq.heapify(hq)
 
 while hq:
     val,idx = heapq.heappop(hq)
-    res.append(max_val - val)
+    if res > max_val - val:
+        res = max_val - val
     if pos[idx] == m:
         break
     heapq.heappush(hq, [nums[idx][pos[idx]], idx])
     max_val = max(max_val, nums[idx][pos[idx]])
     pos[idx] += 1
-print(min(res))
+print(res)
