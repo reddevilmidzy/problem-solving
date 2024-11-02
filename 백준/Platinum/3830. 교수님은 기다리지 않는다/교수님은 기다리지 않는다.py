13848597e80bc2b127ea0f75d4cebc2491676cff
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-11-02 21:19:26 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 1072 ms, Memory: 344040 KB -BaekjoonHub

---
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.py
@@ -0,0 +1,41 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+def find(parent: list[int], x: int) -> int:
+    if parent[x] != x:
+        y = find(parent, parent[x])
+        weight[x] += weight[parent[x]]
+        parent[x] = y
+    return parent[x]
+
+def union(parent: list[int], a: int, b: int, w: int) -> None:
+    pa = find(parent, a)
+    pb = find(parent, b)
+    
+    if pa != pb:
+        weight[pb] = (-weight[b]) + weight[a] + w
+        parent[pb] = pa
+
+res = []
+while True:
+    n,m = map(int,input().split())
+    if n==0: break
+
+    graph = [[] for _ in range(n + 1)]
+    parent = [i for i in range(n + 1)]
+    weight = [0] * (n + 1)
+    hq = []
+    for _ in range(m):
+        cmd = list(input().rstrip().split())
+
+        if cmd[0] == "!": # 업데이트
+            a,b,w = map(int, cmd[1:])
+            union(parent, a, b, w)
+        else:
+            u,v = map(int, cmd[1:])
+            if find(parent, u) != find(parent, v):
+                res.append("UNKNOWN")
+            else:
+                res.append(weight[v] - weight[u])
+print(*res,sep='\n')
