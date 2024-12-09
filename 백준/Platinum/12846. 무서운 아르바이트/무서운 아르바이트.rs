e15aea687c6d8468f52e21d007095779e9766b93
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-12-10 00:22:39 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 8 ms, Memory: 15772 KB -BaekjoonHub

---
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.rs
@@ -0,0 +1,36 @@
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut tokens = stdin.split_whitespace();
+    let mut next = || tokens.next().unwrap();
+
+    let n: usize = next().parse().unwrap();
+    let nums: Vec<usize> = (0..n).map(|_| next().parse().unwrap()).collect();
+
+    let mut r_stk = Vec::new();
+    let mut l_stk = Vec::new();
+
+    let mut r_idx: Vec<i32> = vec![n as i32; n];
+    let mut l_idx: Vec<i32> = vec![-1; n];
+
+    for i in 0..n {
+        while !r_stk.is_empty() && nums[*r_stk.last().unwrap()] > nums[i] {
+            r_idx[r_stk.pop().unwrap()] = i as i32;
+        }
+        r_stk.push(i);
+
+        while !l_stk.is_empty() && nums[*l_stk.last().unwrap()] > nums[n - i - 1] {
+            l_idx[l_stk.pop().unwrap()] = (n - i - 1) as i32;
+        }
+        l_stk.push(n - i - 1);
+    }
+
+    let mut res: i64 = 0;
+
+    for i in 0..n {
+        res = res.max((r_idx[i] as i64 - l_idx[i] as i64 - 1) * nums[i] as i64);
+    }
+
+    print!("{res}")
+}
