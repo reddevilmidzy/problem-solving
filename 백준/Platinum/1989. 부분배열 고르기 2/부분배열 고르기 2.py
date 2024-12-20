dd191c4b7b2622786aad8827e6d0e25797010545
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-12-21 00:30:24 +0900

    [Platinum V] Title: 부분배열 고르기 2, Time: 140 ms, Memory: 142620 KB -BaekjoonHub

---
+++ 백준/Platinum/1989. 부분배열 고르기 2/부분배열 고르기 2.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int,input().split()))
+
+r_stk = []
+r_idx = [n] * n
+l_stk = []
+l_idx = [-1] * n
+
+for i in range(n):
+    while r_stk and nums[r_stk[-1]] > nums[i]:
+        r_idx[r_stk.pop()] = i
+    r_stk.append(i)
+
+    while l_stk and nums[l_stk[-1]] > nums[n - i -1]:
+        l_idx[l_stk.pop()] = n - i - 1
+    l_stk.append(n-i-1)
+
+pre = [0]
+for num in nums:
+    pre.append(pre[-1] + num)
+
+res = [-1,-1,-1]
+
+for i in range(n):
+    tmp = nums[i] * (pre[r_idx[i]] - pre[l_idx[i]+1])
+    if res[0] < tmp:
+        res = [tmp, l_idx[i]+2, r_idx[i]]
+
+print(res[0])
+print(*res[1:])
