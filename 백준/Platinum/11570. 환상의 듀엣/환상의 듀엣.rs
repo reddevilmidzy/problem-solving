554d73cdd79e71b4d2223fd7c49ba016c63eafb2
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-01-23 19:10:32 +0900

    [Platinum V] Title: 환상의 듀엣, Time: 64 ms, Memory: 28908 KB -BaekjoonHub

--- 백준/Platinum/11570. 환상의 듀엣/환상의 듀엣.rs
+++ 백준/Platinum/11570. 환상의 듀엣/환상의 듀엣.rs
@@ -1,15 +1,5 @@
 use std::io::{read_to_string, stdin};
 
-macro_rules! calc_diff {
-    ($i:expr, $j: expr, $nums: expr) => {
-        if $i != 0 {
-            $nums[$i].abs_diff($nums[$j])
-        } else {
-            0
-        }
-    };
-}
-
 fn main() {
     let stdin = read_to_string(stdin()).unwrap();
     let mut token = stdin.split_whitespace();
@@ -38,8 +28,8 @@ fn solve(n: usize, nums: Vec<u32>) -> u32 {
                 continue;
             } else {
                 let nxt = i.max(j) + 1;
-                dp[nxt][j] = dp[nxt][j].min(dp[i][j] + calc_diff!(i, nxt, nums));
-                dp[i][nxt] = dp[i][nxt].min(dp[i][j] + calc_diff!(j, nxt, nums));
+                dp[nxt][j] = dp[nxt][j].min(dp[i][j] + if i != 0 {nums[i].abs_diff(nums[nxt])} else {0});
+                dp[i][nxt] = dp[i][nxt].min(dp[i][j] + if j != 0 {nums[j].abs_diff(nums[nxt])} else {0});
             }
         }
     }
@@ -49,16 +39,4 @@ fn solve(n: usize, nums: Vec<u32>) -> u32 {
         res = res.min(dp[n][i]);
     }
     res
-}
-
-#[test]
-fn tmp() {
-    // assert_eq!(4, solve(3, vec![0, 1, 5, 10]));
-    // assert_eq!(6, solve(4, vec![0, 1, 5, 10, 3]));
-    // solve(4, vec![0, 1, 5, 10, 3]);
-    // solve(5, vec![0, 1,3,8,12,13]);
-    assert_eq!(3, solve(5, vec![0, 1, 5, 6, 2, 1, 0]));
-
-    // solve(2, vec![0, 4, 3]);
-    // solve(3, vec![0, 1, 4, 10]);
 }
