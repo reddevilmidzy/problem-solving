19fba745b61cc4c79eed325b28166646153480bb
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-01-25 16:38:36 +0900

    [Platinum II] Title: 님 게임, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Platinum/11694. 님 게임/님 게임.rs
@@ -0,0 +1,46 @@
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split_whitespace();
+    let mut next = || token.next().unwrap();
+
+    let n: usize = next().parse().unwrap();
+    let nums: Vec<u32> = (0..n).map(|_| next().parse().unwrap()).collect();
+    print!("{}", solve(nums));
+}
+
+fn solve(nums: Vec<u32>) -> String {
+    let mut res = 0u32;
+    let mut cnt = 0u32;
+    for num in nums {
+        if num == 1 {
+            cnt += 1;
+        }
+        res ^= num;
+    }
+
+    if res == 0 {
+        if cnt == 0 || cnt % 2 != 0 {
+            return "cubelover".to_string();
+        }
+        return "koosaga".to_string();
+    } else if res != 1 {
+        return "koosaga".to_string();
+    }
+    "cubelover".to_string()
+}
+
+#[test]
+fn tmp() {
+    let koo = "koosaga".to_string();
+    let cub = "cubelover".to_string();
+    assert_eq!(cub, solve(vec![5,4,1])); // 0
+    assert_eq!(koo, solve(vec![1, 1])); // 0
+    assert_eq!(cub, solve(vec![2, 2])); // 0
+    assert_eq!(cub, solve(vec![9, 8, 9, 8, 9, 9])); // 0
+    assert_eq!(cub, solve(vec![1])); // 1
+    assert_eq!(koo, solve(vec![2])); // 2
+    assert_eq!(koo, solve(vec![1, 2])); // 3
+    assert_eq!(koo, solve(vec![1, 2, 3, 4])); // 4
+}
