26ad86baa3226c024eb5b2d7dba870b2453bc598
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-01 14:01:50 +0900

    [Gold V] Title: 탑, Time: 328 ms, Memory: 216624 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    n = int(input())
-    nums = list(map(int,input().split()))
-    res = [0]*n
-    stk = []
-    for i in range(n):
-        while stk:
-            if nums[i] <= stk[-1][0]:
-                res[i] = stk[-1][1]+1
-                stk.append([nums[i], i])
-                break
-            else:
-                stk.pop()
-        if not stk:
+n = int(input())
+nums = list(map(int,input().split()))
+res = [0]*n
+stk = []
+for i in range(n):
+    while stk:
+        if nums[i] <= stk[-1][0]:
+            res[i] = stk[-1][1]+1
             stk.append([nums[i], i])
-    return res
+            break
+        else:
+            stk.pop()
+    if not stk:
+        stk.append([nums[i], i])
 
-if __name__ == "__main__":
-    print(*solve(), sep=' ')
+print(*res, sep=' ')
