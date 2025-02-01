cdd50a4dcb0cb1bd77e147a78b8b2ebf4a5a0d2f
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-01 19:10:10 +0900

    [Platinum IV] Title: 멀티탭 스케줄링 2, Time: 240 ms, Memory: 60988 KB -BaekjoonHub

--- 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.rs
+++ 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::collections::{BinaryHeap, VecDeque};
 use std::io::{read_to_string, stdin};
 
 fn main() {
@@ -16,28 +16,25 @@ fn main() {
 
 fn solve(n: usize, k: usize, nums: Vec<u32>) -> i32 {
     let mut res = 0;
-    let mut idx = HashMap::new();
+    let mut idx = vec![VecDeque::new(); k + 1];
     let mut used = vec![false; k + 1];
 
     for i in 0..k {
-        if !idx.contains_key(&nums[i]) {
-            idx.insert(nums[i], VecDeque::new());
-        }
-        idx.get_mut(&nums[i]).unwrap().push_back(i);
+        idx[nums[i] as usize].push_back(i);
     }
 
     let mut hq = BinaryHeap::with_capacity(n);
     let mut cnt = 0;
 
     for i in 0..k {
-        idx.get_mut(&nums[i]).unwrap().pop_front();
-        let val = *idx.get(&nums[i]).unwrap().front().unwrap_or(&k);
+        idx[nums[i] as usize].pop_front();
+        let val = *idx[nums[i] as usize].front().unwrap_or(&k);
         if cnt >= n && !used[nums[i] as usize] {
             res += 1;
 
             loop {
                 let (_p_idx, p_val) = hq.pop().unwrap();
-                if used[p_val as usize]{
+                if used[p_val as usize] {
                     used[p_val as usize] = false;
                     cnt -= 1;
                     break;
@@ -60,6 +57,6 @@ fn tmp() {
     assert_eq!(5, solve(3, 8, vec![1, 2, 3, 4, 5, 6, 7, 8]));
     assert_eq!(2, solve(1, 3, vec![2, 1, 2]));
     assert_eq!(4, solve(2, 8, vec![1, 2, 3, 4, 1, 3, 4, 2]));
-    assert_eq!(4, solve(2, 9, vec![1,2,3,1,2,3,1,2,3]));
+    assert_eq!(4, solve(2, 9, vec![1, 2, 3, 1, 2, 3, 1, 2, 3]));
     assert_eq!(0, solve(2, 10, vec![1, 1, 2, 1, 1, 2, 1, 1, 2, 1]));
 }
