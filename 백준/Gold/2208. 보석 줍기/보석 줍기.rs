e44ebdca9d0853a6d97b0d0224be6acd7718a7e8
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-03 15:19:33 +0900

    [Gold II] Title: 보석 줍기, Time: 8 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.rs
@@ -0,0 +1,60 @@
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split_whitespace();
+    let mut next = || token.next().unwrap();
+
+    let n: usize = next().parse().unwrap();
+    let m: usize = next().parse().unwrap();
+    let nums: Vec<i32> = (0..n).map(|_| next().parse().unwrap()).collect();
+    print!("{}", solve(n, m, nums));
+}
+
+fn solve(n: usize, m: usize, nums: Vec<i32>) -> i32 {
+    let mut cnt = vec![1; n];
+    let mut dp = vec![i32::MIN; n];
+    let mut pre = vec![0; n + 1];
+
+    for i in 1..=n {
+        pre[i] = pre[i - 1] + nums[i - 1];
+    }
+
+    dp[0] = nums[0];
+
+    let mut satisfied = vec![-1_000_000; n];
+
+    for i in 1..n {
+        let val = if i + 1 >= m {
+            pre[i + 1] - pre[i + 1 - m]
+        } else {
+            -1_000_000
+        };
+
+        satisfied[i] = satisfied[i].max(val).max(satisfied[i - 1] + nums[i]);
+
+        if nums[i] < dp[i - 1] + nums[i] {
+            dp[i] = dp[i - 1] + nums[i];
+            cnt[i] = cnt[i - 1] + 1;
+        } else {
+            dp[i] = nums[i];
+        }
+    }
+
+    let mut res = 0;
+    for i in 0..n {
+        if cnt[i] >= m {
+            res = res.max(dp[i]);
+        }
+        res = res.max(satisfied[i]);
+    }
+    res
+}
+
+#[test]
+fn tmp() {
+    assert_eq!(5, solve(8, 4, vec![-1, -1, 1, 1, 1, 1, -1, 2]));
+    assert_eq!(1, solve(5, 5, vec![-1, -1, 1, 1, 1]));
+    assert_eq!(1, solve(6, 4, vec![-8, -1, -3, 6, -1, -9]));
+    assert_eq!(5, solve(7, 1, vec![-1, 1, 1, 1, 1, 1, -1]));
+}
