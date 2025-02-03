064ac20258d165f413c695939e404d3fc044eb34
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-03 15:41:19 +0900

    [Gold II] Title: 보석 줍기, Time: 4 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/2208. 보석 줍기/보석 줍기.rs
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.rs
@@ -7,40 +7,22 @@ fn main() {
 
     let n: usize = next().parse().unwrap();
     let m: usize = next().parse().unwrap();
-    let nums: Vec<i32> = (0..n).map(|_| next().parse().unwrap()).collect();
+    let mut nums: Vec<i32> = vec![0; n + 1];
+    for i in 1..=n {
+        nums[i] = next().parse().unwrap();
+        nums[i] += nums[i-1];
+    }
     print!("{}", solve(n, m, nums));
 }
 
 fn solve(n: usize, m: usize, nums: Vec<i32>) -> i32 {
-    let mut pre = vec![0; n + 1];
-
-    for i in 1..=n {
-        pre[i] = pre[i - 1] + nums[i - 1];
-    }
-
-    let mut satisfied = vec![-1_000_000; n];
-
-    for i in 1..n {
-        let val = if i + 1 >= m {
-            pre[i + 1] - pre[i + 1 - m]
-        } else {
-            -1_000_000
-        };
-
-        satisfied[i] = val.max(satisfied[i - 1] + nums[i]);
-    }
 
     let mut res = 0;
-    for i in 0..n {
-        res = res.max(satisfied[i]);
+    let mut cur = 0;
+    for i in m..=n {
+        cur = cur.min(nums[i - m]);
+        res = res.max(nums[i] - cur);
     }
-    res
-}
 
-#[test]
-fn tmp() {
-    assert_eq!(5, solve(8, 4, vec![-1, -1, 1, 1, 1, 1, -1, 2]));
-    assert_eq!(1, solve(5, 5, vec![-1, -1, 1, 1, 1]));
-    assert_eq!(1, solve(6, 4, vec![-8, -1, -3, 6, -1, -9]));
-    assert_eq!(5, solve(7, 1, vec![-1, 1, 1, 1, 1, 1, -1]));
+    res
 }
