1a78795df1e28874db81af0e721f1605e8e048a6
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-05 22:01:41 +0900

    [Gold V] Title: 평범한 배낭, Time: 212 ms, Memory: 111396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,13 +3,11 @@ input = sys.stdin.readline
 
 n,k = map(int,input().split())
 back =[[0,0]]+ [list(map(int,input().split())) for _ in range(n)]
-dp = [[0]*(k+1) for _ in range(n+1)]
+dp = [0]*(k+1)
+
 for i in range(1, n+1):
-    for j in range(1, k+1):
-        if j - back[i][0] >= 0:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-back[i][0]]+back[i][1])
-            
-        else:
-            dp[i][j] = dp[i-1][j]
-        
-print(dp[n][k])
+    for j in range(k, 0, -1):
+        if back[i][0] <= j:
+            dp[j] = max(dp[j], dp[j-back[i][0]]+back[i][1])
+
+print(dp[k])
