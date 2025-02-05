ee844aa6f2a4d7cf34abe45c2aa39a86a12c5b70
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-05 12:39:48 +0900

    [Gold I] Title: Dr Who’s Banquet, Time: 68 ms, Memory: 23480 KB -BaekjoonHub

---
+++ 백준/Gold/11421. Dr Who’s Banquet/Dr Who’s Banquet.rs
@@ -0,0 +1,70 @@
+use std::collections::BinaryHeap;
+use std::fmt::Write;
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split("\n");
+    let mut next = || token.next();
+
+    let mut res = String::new();
+    while let Some(input) = next() {
+        if input.is_empty() {
+            break;
+        }
+        let nums: Vec<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
+        let n = nums.len();
+        writeln!(res, "{}", solve(n, nums)).unwrap();
+    }
+    print!("{res}");
+}
+
+fn solve(n: usize, nums: Vec<i32>) -> String {
+    let mut res = String::new();
+    let mut hq = BinaryHeap::with_capacity(n);
+
+    let mut even = 0;
+    for i in 0..n {
+        even += nums[i];
+        if nums[i] > 0 {
+            hq.push((nums[i], i));
+        } else if nums[i] >= n as i32 {
+            writeln!(res, "fail").unwrap();
+            return res;
+        }
+    }
+    // 총합이 짝수여야 한다.
+    if even % 2 != 0 {
+        writeln!(res, "fail").unwrap();
+        return res;
+    }
+
+    let mut graph = vec![vec![0; n]; n];
+
+    while let Some((cur, i)) = hq.pop() {
+        // 현재 차수만큼 젤 큰놈 끄집어냄
+        if hq.len() < cur as usize {
+            writeln!(res, "fail").unwrap();
+            return res;
+        }
+
+        let mut tmp = BinaryHeap::with_capacity(cur as usize);
+        for _ in 0..cur {
+            let (nxt, j) = hq.pop().unwrap();
+            graph[i][j] = 1;
+            graph[j][i] = 1;
+            if nxt > 1 {
+                tmp.push((nxt - 1, j));
+            }
+        }
+        hq.append(&mut tmp);
+    }
+
+    for i in 0..n {
+        for j in 0..n {
+            write!(res, "{} ", graph[i][j]).unwrap();
+        }
+        writeln!(res, "").unwrap();
+    }
+    res
+}
---
+++ 백준/Gold/11421. Dr Who’s Banquet/README.md
@@ -0,0 +1,28 @@
+# [Gold I] Dr Who’s Banquet - 11421 
+
+[문제 링크](https://www.acmicpc.net/problem/11421) 
+
+### 성능 요약
+
+메모리: 23480 KB, 시간: 68 ms
+
+### 분류
+
+해 구성하기, 차수열, 그래프 이론, 그리디 알고리즘, 정렬
+
+### 제출 일자
+
+2025년 2월 5일 12:39:42
+
+### 문제 설명
+
+<p>Dr. Who is organising a banquet, and will be inviting several guests. A guest is happy, if he can chat with a fixed number of other guests. We assume that guests cannot talk to themselves. Help Dr. Who make all his guests happy, if possible, by organising chats between guests.</p>
+
+### 입력 
+
+ <p>The program input is from a text file. The file contains several data sets, and each data set is encoded on a line from the file. A data set consists of n≤10000 positive integers a<sub>1</sub>, a<sub>2</sub>, … a<sub>n</sub>, separated by single whitespaces. The last integer an is immediately followed by the newline character. Each number a<sub>i</sub>, with 1≤i≤n is the number of chat partners guest i would like to have. We assume that a<sub>i</sub>≤1000 for all 1≤i≤n. The last data set is followed by the end of file.</p>
+
+### 출력 
+
+ <p>If all guests can be made happy, the program output consists of a n×n matrix m, where m[i][j]=m[j][i]=1 if guests i and j chat, and m[i][j]=m[j][i]=0, otherwise. The matrix will be represented at standard output, as follows: each value m[i][j] from a row will be followed by one whitespace (including the last value from the row). Each row will be separated by the newline character. If it is not possible for all guests to be happy, then the program output is the message “fail”. The matrix and the message are allways followed by an empty line.</p>
+
