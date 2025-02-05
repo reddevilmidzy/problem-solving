fce0fd83c9f04edfbc82e62973d0366077f3e70f
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-06 00:27:11 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,9 +1,2 @@
 def solution(left, right):
-    answer = 0
-    for i in range(left, right + 1):
-        if int(i**0.5)**2 == i:
-            answer -= i
-        else:
-            answer += i
-        
-    return answer
+    return sum([i * (1 + -2 * (int(i**0.5)**2 == i)) for i in range(left, right + 1)])
