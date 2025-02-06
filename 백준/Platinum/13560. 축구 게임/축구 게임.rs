3dc797340fd366bce03a83a7fbf9a201e7f565fc
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-06 18:49:03 +0900

    [Platinum V] Title: 축구 게임, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Platinum/13560. 축구 게임/축구 게임.rs
+++ 백준/Platinum/13560. 축구 게임/축구 게임.rs
@@ -1,4 +1,3 @@
-use std::collections::BinaryHeap;
 use std::io::{read_to_string, stdin};
 
 fn main() {
@@ -11,39 +10,19 @@ fn main() {
     print!("{}", solve(n, nums));
 }
 
-fn solve(n: usize, nums: Vec<u32>) -> i8 {
-    let mut winner = BinaryHeap::with_capacity(n);
-    let mut loser = BinaryHeap::with_capacity(n);
+fn solve(n: usize, mut nums: Vec<u32>) -> i8 {
     let mut tot = 0;
-    for i in 0..n {
+    nums.sort();
+    let mut left = 0;
+    for i in (0..n).rev() {
         tot += nums[i];
-        if nums[i] > 0 {
-            winner.push(nums[i]);
-        }
-        if nums[i] < n as u32 - 1 {
-            loser.push((n as u32 - 1) - nums[i]);
-        }
-    }
-
-    if tot != ((n - 1) * n / 2) as u32 {
-        return -1;
-    }
-
-    while let Some(cur) = winner.pop() {
-        if loser.len() < cur as usize {
+        if nums[i] > left + i as u32 {
             return -1;
         }
-
-        let mut tmp = BinaryHeap::with_capacity(cur as usize);
-
-        for _ in 0..cur {
-            let nxt = loser.pop().unwrap();
-            // i j 같으면 어카지
-            if nxt > 1 {
-                tmp.push(nxt - 1);
-            }
-        }
-        loser.append(&mut tmp);
+        left = left + i as u32 - nums[i];
+    }
+    if tot != ((n - 1) * n / 2) as u32 {
+        return -1;
     }
 
     1
