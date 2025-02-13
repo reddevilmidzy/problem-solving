5afd775135941f83fc7c2d9c1f6938e94e9e26d1
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-13 23:55:14 +0900

    [Gold IV] Title: 돌 그룹, Time: 352 ms, Memory: 148080 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def nxt(x: int, y: int) -> tuple[int]:
+    if x == y:
+        return (x, y)
+    min_v = min(x, y)
+    max_v = max(x, y)
+
+    return (min_v + min_v, max_v - min_v)
+
+nums = tuple(sorted(list(map(int,input().split()))))
+
+def bfs(a:int, b:int, c:int):
+    queue = deque()
+    queue.append((a,b,c))
+    visited = set()
+    visited.add((a,b,c))
+
+    while queue:
+        cur = queue.popleft()
+        if cur[0] == cur[2]:
+            return 1
+        for i,j,k in ((0,1,2), (1,2,0), (0,2,1)):
+            y,x = nxt(cur[i], cur[j])
+            tmp = tuple(sorted((y,x,cur[k])))
+            if tmp not in visited:
+                visited.add(tmp)
+                queue.append(tmp)
+    return 0
+
+print(bfs(*nums))
