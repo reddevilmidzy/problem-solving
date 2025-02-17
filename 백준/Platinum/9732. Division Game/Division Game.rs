1ff26b07b205934e482afa206612387aed1c26a3
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-18 00:05:57 +0900

    [Platinum III] Title: Division Game, Time: 12 ms, Memory: 13344 KB -BaekjoonHub

--- 백준/Platinum/9732. Division Game/Division Game.rs
+++ 백준/Platinum/9732. Division Game/Division Game.rs
@@ -22,93 +22,58 @@ fn main() {
     print!("{res}");
 }
 
-fn get_factor(num: u32) -> Vec<u32> {
-    let mut res = Vec::new();
-    // res.push(1);
-    res.push(num);
+fn get_factor_cnt() -> Vec<u32> {
+    let m = 10000;
+    let mut res = vec![0; m + 1];
 
-    for i in 2..=(num as f32).sqrt() as u32 {
-        if num % i == 0 {
-            res.push(i);
-            if num / i != i {
-                res.push(num / i);
+    let mut is_prime = vec![true; m + 1];
+
+    for i in 2..=m {
+        if is_prime[i] {
+            for j in (i..=m).step_by(i) {
+                let mut x = j;
+                let mut cnt = 0;
+                while x % i == 0 {
+                    x /= i;
+                    cnt += 1;
+                }
+
+                res[j] += cnt;
+                is_prime[j] = false;
             }
         }
     }
-    res.sort();
-
     res
 }
 
+
 fn solve(n: usize, m: usize, nums: Vec<Vec<u32>>) -> String {
-    let grundy = get_grundy();
-    // 내가 동시에 취할 수 있는 행동은 xor 시키고.
-    // 한 턴에서 할 수 있는 그런디 수를 확인하기 위해 현재 상태에서 갈 수 있는 상태 값들의 mex를 구한다.
+    let cnt = get_factor_cnt();
 
     let mut res = 0;
-    let mut flag = false;
 
     for i in 0..n {
         // 이번 턴에서 할 수 있느 것
         let mut add = 0;
 
         for j in 0..m {
-            // nums[i][j] 의 팩터를 구한 다음
-            // nums[i] 의 모든 수의 팩터를 구한 다음 각 조합을 xor 시킨 후 mex를 취한 것이 그런디 수
-            add += grundy[nums[i][j] as usize];
-            // print!("{} ", grundy[nums[i][j] as usize]);
+            add += cnt[nums[i][j] as usize];
         }
 
-        flag |= add != 1;
         res ^= add;
-
     }
 
-    if (res == 0) ^ flag {
+    if res != 0 {
         "YES".to_string()
     } else {
         "NO".to_string()
     }
 }
 
-fn mex(num: u16) -> u16 {
-    for i in 0..=15 {
-        if num & (1 << i) == 0 {
-            return i as u16;
-        }
-    }
-    16
-}
-
-fn get_grundy() -> Vec<u16> {
-    let m = 10000;
-    let mut grundy = vec![0; m + 1];
-
-    for i in 2..=m {
-        let factors = get_factor(i as u32);
-        let mut bit = 0;
-
-        for factor in factors {
-            bit |= 1 << grundy[factor as usize];
-        }
-
-        grundy[i] = mex(bit);
-    }
-
-    grundy
-}
 
 #[test]
 fn tmp() {
-
-    // let val = get_grundy();
-    // println!("{:?}", val);
-    // println!("{}", solve(2,2,vec![vec![2,3], vec![2,3]]));
-
-
-    let val = solve(2,3,vec![vec![4,5,6],vec![7,8,9]]);
+    let val = solve(2, 3, vec![vec![4, 5, 6], vec![7, 8, 9]]);
 
     print!("{}", val);
 }
-
-// [0, 0, 1, 1, 2, 1, 2, 1, 3, 2, 2, 1, 3, 1, 2, 2, 4, 1, 3, 1, 3, 2, 2, 1, 4, 2, 2, 3, 3, 1, 3, 1, 5, 2, 2, 2, 4, 1, 2, 2, 4, 1, 3, 1, 3, 3, 2, 1, 5, 2, 3]
