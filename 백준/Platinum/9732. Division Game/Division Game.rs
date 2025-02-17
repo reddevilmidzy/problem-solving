5db23fac2b6c529b323a7842805f803cfc170091
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-18 00:41:44 +0900

    [Platinum III] Title: Division Game, Time: 0 ms, Memory: 13344 KB -BaekjoonHub

--- 백준/Platinum/9732. Division Game/Division Game.rs
+++ 백준/Platinum/9732. Division Game/Division Game.rs
@@ -7,7 +7,8 @@ fn main() {
     let mut next = || token.next().unwrap();
 
     let mut res = String::new();
-    let grundy = get_grundy();
+
+    let cnt = get_factor_cnt();
 
     let t = next().parse().unwrap();
     for i in 1..=t {
@@ -18,81 +19,51 @@ fn main() {
             .map(|_| (0..m).map(|_| next().parse::<u32>().unwrap()).collect())
             .collect();
 
-        writeln!(res, "Case #{i}: {}", solve(n, m, nums, &grundy)).unwrap();
+        writeln!(res, "Case #{i}: {}", solve(n, m, nums, &cnt)).unwrap();
     }
     print!("{res}");
 }
 
-fn get_factor(num: u32) -> Vec<u32> {
-    let mut res = Vec::new();
-    // res.push(1);
-    res.push(num);
+fn get_factor_cnt() -> Vec<u32> {
+    let m = 10000;
+    let mut res = vec![0; m + 1];
 
-    for i in 2..=(num as f32).sqrt() as u32 {
-        if num % i == 0 {
-            res.push(i);
-            if num / i != i {
-                res.push(num / i);
+    let mut is_prime = vec![true; m + 1];
+
+    for i in 2..=m {
+        if is_prime[i] {
+            for j in (i..=m).step_by(i) {
+                let mut x = j;
+                let mut cnt = 0;
+                while x % i == 0 {
+                    x /= i;
+                    cnt += 1;
+                }
+
+                res[j] += cnt;
+                is_prime[j] = false;
             }
         }
     }
-    res.sort();
-
     res
 }
 
-fn solve(n: usize, m: usize, nums: Vec<Vec<u32>>, grundy: &Vec<u16>) -> String {
-    // let grundy = get_grundy();
-    // 내가 동시에 취할 수 있는 행동은 xor 시키고.
-    // 한 턴에서 할 수 있는 그런디 수를 확인하기 위해 현재 상태에서 갈 수 있는 상태 값들의 mex를 구한다.
-
+fn solve(n: usize, m: usize, nums: Vec<Vec<u32>>, cnt: &Vec<u32>) -> String {
     let mut res = 0;
-    let mut flag = false;
 
     for i in 0..n {
-        // 이번 턴에서 할 수 있느 것
         let mut add = 0;
 
         for j in 0..m {
-            add += grundy[nums[i][j] as usize];
-            // print!("{} ", grundy[nums[i][j] as usize]);
+            add += cnt[nums[i][j] as usize];
         }
 
-        flag |= add != 1;
         res ^= add;
-
     }
 
-    if (res == 0) ^ flag {
+    if res != 0 {
         "YES".to_string()
     } else {
         "NO".to_string()
     }
-}
-
-fn mex(num: u16) -> u16 {
-    for i in 0..=15 {
-        if num & (1 << i) == 0 {
-            return i as u16;
-        }
-    }
-    16
-}
-
-fn get_grundy() -> Vec<u16> {
-    let m = 10000;
-    let mut grundy = vec![0; m + 1];
-
-    for i in 2..=m {
-        let factors = get_factor(i as u32);
-        let mut bit = 0;
-
-        for factor in factors {
-            bit |= 1 << grundy[factor as usize];
-        }
-
-        grundy[i] = mex(bit);
-    }
-
-    grundy
 }
