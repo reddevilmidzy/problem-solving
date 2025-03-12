b0d917c340809e78447faffb3a6a5513c921c64e
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-03-12 20:56:19 +0900

    Create imos_2D.rs

---
+++ snnipet/imos_2D.rs
@@ -0,0 +1,40 @@
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split_whitespace();
+    let mut next = || token.next().unwrap();
+
+    let m = 1000;
+    let n: usize = next().parse().unwrap();
+
+    let mut pre = vec![vec![0; m + 1]; m + 1];
+    for _ in 0..n {
+        let (x1, y1, x2, y2): (usize, usize, usize, usize) = (
+            next().parse().unwrap(),
+            next().parse().unwrap(),
+            next().parse().unwrap(),
+            next().parse().unwrap(),
+        );
+
+        pre[y1][x1] += 1;
+        pre[y2][x2] += 1;
+
+        pre[y1][x2] -= 1;
+        pre[y2][x1] -= 1;
+    }
+
+    for i in 0..=m {
+        for j in 0..=m {
+            if i + j == 0 {
+                continue;
+            } else if i == 0 {
+                pre[i][j] += pre[i][j - 1];
+            } else if j == 0 {
+                pre[i][j] += pre[i - 1][j];
+            } else {
+                pre[i][j] += pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+            }
+        }
+    }
+}
