faf45bf9028356cdd33f76c2e161a3b3749b9956
Redddy <midzy0228@gmail.com>
2025-04-15 12:32:03 +0900

    [Gold V] Title: 사과나무, Time: 68 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Gold/20002. 사과나무/사과나무.rs
@@ -0,0 +1,36 @@
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split_whitespace();
+    let mut next = || token.next().unwrap();
+
+    let n: usize = next().parse().unwrap();
+    let board: Vec<Vec<i32>> = (0..n)
+        .map(|_| (0..n).map(|_| next().parse::<i32>().unwrap()).collect())
+        .collect();
+
+    let mut pre = vec![vec![0; n + 1]; n + 1];
+
+    for i in 1..=n {
+        for j in 1..=n {
+            pre[i][j] = pre[i - 1][j] + pre[i][j - 1] + board[i - 1][j - 1] - pre[i - 1][j - 1];
+        }
+    }
+
+    let mut res = i32::MIN;
+
+    for k in 0..n {
+        for i in 1..=n {
+            for j in 1..=n {
+                if i + k > n || j + k > n {
+                    continue;
+                }
+                let tmp =
+                    pre[i + k][j + k] - pre[i - 1][j + k] - pre[i + k][j - 1] + pre[i - 1][j - 1];
+                res = res.max(tmp);
+            }
+        }
+    }
+    print!("{res}");
+}
