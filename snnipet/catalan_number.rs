129859c8d6cbd948a99d8c4afd15eca84c39d6ad
Redddy <areyou@redddy.com>
2025-04-26 12:43:29 +0900

    Create catalan_number.rs

---
+++ snnipet/catalan_number.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let stdin = read_to_string(stdin()).unwrap();
+    let mut token = stdin.split_whitespace();
+    let mut next = || token.next().unwrap();
+    let mut res = String::new();
+
+    let n: usize = 1_000;
+    const MOD: u64 = 1_000_000_007;
+    let mut dp = vec![0u64; n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for i in 1..=n {
+        let mut tmp = 0u64;
+        for j in 0..i {
+            tmp = ((tmp % MOD) + (dp[j] * dp[i - j]) % MOD) % MOD;
+        }
+        dp[i] = tmp;
+    }
+}
